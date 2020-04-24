d08c57789b33d8227eed06900a61ca79b076fa9e
Alexander Smarus <bender@readdle.com>
2020-04-25 01:24:06 +0300

    [script] add missing environment variable

--- scripts/workflows/swift-master.cmd
+++ scripts/workflows/swift-master.cmd
@@ -10,6 +10,7 @@ set SW_INSTALL_DIR=%SW_WORKSPACE%\install
 set SW_JOBS_DIR=%SW_WORKSPACE%\scripts\workflows\jobs
 set SW_ICU_VERSION=64
 set SW_ZLIB_VERSION=1.2.11
+set SW_XML2_VERSION=development
 
 call %SW_JOBS_DIR%\icu.cmd^
  && call %SW_JOBS_DIR%\zlib.cmd^
