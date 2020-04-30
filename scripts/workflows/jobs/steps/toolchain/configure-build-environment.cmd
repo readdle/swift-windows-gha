305454323138236062ace7133cdb14891ff25a08
Alexander Smarus <bender@readdle.com>
2020-05-01 01:10:46 +0300

    [toolchain] configure build environment prior to other steps

--- scripts/workflows/jobs/steps/toolchain/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/toolchain/configure-build-environment.cmd
@@ -1,10 +1,26 @@
-call scripts\tools\vsenv.cmd -arch=x64 -host_arch=x64
-          
+if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
+  set SW_LLVM_REF=swift/master
+  set SW_CMARK_REF=master
+  set SW_LIBDISPATCH_REF=master
+  set SW_SWIFT_REF=master
+
+  set SW_LLDB_OPTIONS=-DLLDB_DISABLE_PYTHON=YES
+) else (
+  set SW_LLVM_REF=swift/swift-5.2-branch
+  set SW_CMARK_REF=swift-5.2-branch
+  set SW_LIBDISPATCH_REF=swift-5.2-branch
+  set SW_SWIFT_REF=swift-5.2-branch
+)
+
 set SW_TOOLCHAIN_INSTALL_DIR=%SW_INSTALL_DIR%\Library\Developer\Toolchains\unknown-Asserts-development.xctoolchain
 set SW_ICU_PATH=%SW_ARTIFACTS_DIR%\Library\icu-%SW_ICU_VERSION%
 
-if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] SET SW_LLDB_OPTIONS=-DLLDB_DISABLE_PYTHON=YES
+call scripts\tools\vsenv.cmd -arch=x64 -host_arch=x64
 
+echo ::set-env name=SW_LLVM_REF::%SW_LLVM_REF%
+echo ::set-env name=SW_CMARK_REF::%SW_CMARK_REF%
+echo ::set-env name=SW_LIBDISPATCH_REF::%SW_LIBDISPATCH_REF%
+echo ::set-env name=SW_SWIFT_REF::%SW_SWIFT_REF%
 echo ::set-env name=SW_TOOLCHAIN_INSTALL_DIR::%SW_TOOLCHAIN_INSTALL_DIR%
 echo ::set-env name=SW_ICU_PATH::%SW_ICU_PATH%
 
--- scripts/workflows/jobs/steps/toolchain/configure-test-environment.cmd
+++ scripts/workflows/jobs/steps/toolchain/configure-test-environment.cmd
@@ -1,1 +1,1 @@
-echo ::set-env name=PATH::%SW_ICU_PATH%\usr\bin;%SW_TOOLCHAIN_INSTALL_DIR%\usr\bin;%PATH%;%ProgramFiles%\Git\usr\bin
+echo ::set-env name=PATH::%SW_ICU_PATH%\usr\bin;%SW_TOOLCHAIN_INSTALL_DIR%\bin;%PATH%;%ProgramFiles%\Git\usr\bin
