4a20ba82179dc236a4bdc5adfd25ca2d43af4286
Alexander Smarus <bender@readdle.com>
2020-05-01 16:06:39 +0300

    [toolchain] configure source directories in env

--- scripts/workflows/jobs/steps/toolchain/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/toolchain/configure-build-environment.cmd
@@ -1,26 +1,39 @@
 if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
   set SW_LLVM_REF=swift/swift-5.2-branch
   set SW_CMARK_REF=swift-5.2-branch
-  set SW_LIBDISPATCH_REF=swift-5.2-branch
+  set SW_DISPATCH_REF=swift-5.2-branch
   set SW_SWIFT_REF=swift-5.2-branch
 
   set SW_LLDB_OPTIONS=-DLLDB_DISABLE_PYTHON=YES
 ) else (
   set SW_LLVM_REF=swift/master
   set SW_CMARK_REF=master
-  set SW_LIBDISPATCH_REF=master
+  set SW_DISPATCH_REF=master
   set SW_SWIFT_REF=master
 )
+
+set SW_LLVM_SOURCES_DIR=%SW_SOURCES_DIR%\toolchain
+set SW_CMARK_SOURCES_DIR=%SW_LLVM_SOURCES_DIR%\cmark
+set SW_SWIFT_SOURCES_DIR=%SW_LLVM_SOURCES_DIR%\swift
+set SW_DISPATCH_SOURCES_DIR=%SW_SOURCES_DIR%\swift-corelibs-libdispatch
 
 set SW_TOOLCHAIN_INSTALL_DIR=%SW_INSTALL_DIR%\Library\Developer\Toolchains\unknown-Asserts-development.xctoolchain\usr
 set SW_ICU_PATH=%SW_ARTIFACTS_DIR%\Library\icu-%SW_ICU_VERSION%
 
 call scripts\tools\vsenv.cmd -arch=x64 -host_arch=x64
 
+if not defined GITHUB_ACTION goto :eof
+
 echo ::set-env name=SW_LLVM_REF::%SW_LLVM_REF%
 echo ::set-env name=SW_CMARK_REF::%SW_CMARK_REF%
-echo ::set-env name=SW_LIBDISPATCH_REF::%SW_LIBDISPATCH_REF%
+echo ::set-env name=SW_DISPATCH_REF::%SW_DISPATCH_REF%
 echo ::set-env name=SW_SWIFT_REF::%SW_SWIFT_REF%
+
+echo ::set-env name=SW_LLVM_SOURCES_DIR::%SW_LLVM_SOURCES_DIR%
+echo ::set-env name=SW_CMARK_SOURCES_DIR::%SW_CMARK_SOURCES_DIR%
+echo ::set-env name=SW_SWIFT_SOURCES_DIR::%SW_SWIFT_SOURCES_DIR%
+echo ::set-env name=SW_DISPATCH_SOURCES_DIR::%SW_DISPATCH_SOURCES_DIR%
+
 echo ::set-env name=SW_TOOLCHAIN_INSTALL_DIR::%SW_TOOLCHAIN_INSTALL_DIR%
 echo ::set-env name=SW_ICU_PATH::%SW_ICU_PATH%
 
