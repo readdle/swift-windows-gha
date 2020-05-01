a7b90f85fbf61cd49a88adef465dbe92fd582dd8
Alexander Smarus <bender@readdle.com>
2020-05-01 22:58:54 +0300

    [toolchain] restore deleted toolchain config script

--- scripts/workflows/jobs/steps/toolchain/expose-build-tools.cmd
+++
@@ -1,3 +0,0 @@
-echo ::set-env name=LLVM_TABLEGEN::%SW_BUILD_DIR%\llvm-tools\bin\llvm-tblgen.exe
-echo ::set-env name=CLANG_TABLEGEN::%SW_BUILD_DIR%\llvm-tools\bin\clang-tblgen.exe
-echo ::set-env name=LLDB_TABLEGEN::%SW_BUILD_DIR%\llvm-tools\bin\lldb-tblgen.exe
