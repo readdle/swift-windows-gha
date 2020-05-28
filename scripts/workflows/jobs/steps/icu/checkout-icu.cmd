6b8c8d84401f7aa2ed6172f5afebcddbcd8bdc5e
Alexander Smarus <bender@readdle.com>
2020-05-29 01:18:38 +0300

    [icu] try to upgrade to v67

--- scripts/workflows/jobs/steps/icu/checkout-icu.cmd
+++ scripts/workflows/jobs/steps/icu/checkout-icu.cmd
@@ -8,7 +8,10 @@ git init %SW_ICU_SOURCES_DIR%^
  && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_ICU_REF%*:refs/remotes/origin/%SW_ICU_REF%* +refs/tags/%SW_ICU_REF%*:refs/tags/%SW_ICU_REF%*^
  && git branch --list --remote origin/%SW_ICU_REF%^
  && git checkout --progress --force -B %SW_ICU_REF% refs/remotes/origin/%SW_ICU_REF%^
- && git log -1
+ && git log -1^
+ && git config user.name 'builder'^
+ && git config user.email 'builder@lxbndr.org'^
+ && git cherry-pick 715d254a02b0b22681cb6f861b0921ae668fa7d6
 
 set SW_ERROR=%ERRORLEVEL%
 popd
--- scripts/workflows/swift-master.cmd
+++ scripts/workflows/swift-master.cmd
@@ -12,7 +12,7 @@ set SW_ARTIFACTS_DIR=%SW_INSTALL_DIR%
 
 set SW_JOBS_DIR=%SW_WORKSPACE%\scripts\workflows\jobs
 set SW_CURL_VERSION=development
-set SW_ICU_VERSION=64
+set SW_ICU_VERSION=67
 set SW_XML2_VERSION=development
 set SW_ZLIB_VERSION=1.2.11
 set SW_SWIFT_BRANCH_SPEC=master
