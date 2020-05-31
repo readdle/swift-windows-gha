b8e629e2be7a8b8f91ef39d03d4c82d799ce41b5
Alexander Smarus <bender@readdle.com>
2020-05-31 14:33:44 +0300

    [icu] fix checkout in script

--- scripts/workflows/jobs/steps/curl/checkout-curl.cmd
+++ scripts/workflows/jobs/steps/curl/checkout-curl.cmd
@@ -5,8 +5,10 @@ pushd %CD%
 git init %SW_CURL_SOURCES_DIR%^
  && cd %SW_CURL_SOURCES_DIR%^
  && (git remote add origin https://github.com/curl/curl.git || ver>nul)^
- && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +%SW_CURL_REF%*:%SW_CURL_REF%*^
- && git checkout --progress --force %SW_CURL_REF%^
+ && git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_CURL_REF%*:refs/remotes/origin/%SW_CURL_REF%* +refs/tags/%SW_CURL_REF%*:refs/tags/%SW_CURL_REF%*^
+ && git branch --list --remote origin/%SW_CURL_REF%^
+ && git tag --list %SW_CURL_REF%^
+ && git checkout --progress --force refs/tags/%SW_CURL_REF%^
  && git log -1
 
 set SW_ERROR=%ERRORLEVEL%
