fcf476b643e077dbccde1745c699390760cdd58f
Alexander Smarus <bender@readdle.com>
2020-06-01 14:03:22 +0300

    [script] verbose patch, investigate CI issue

--- scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
@@ -3,10 +3,11 @@ setlocal
 pushd %CD%
 
 if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
+  echo Working in %SW_LLVM_PROJECT_SOURCES_DIR%
   cd %SW_LLVM_PROJECT_SOURCES_DIR%^
    && git config user.name 'builder'^
    && git config user.email 'builder@lxbndr.org'^
-   && git apply %SW_WORKSPACE%\patch\llvm-project\b288d90b39f4b905c02092a9bfcfd6d78f99b191.patch
+   && git apply --verbose %SW_WORKSPACE%\patch\llvm-project\b288d90b39f4b905c02092a9bfcfd6d78f99b191.patch
 )
 
 set SW_ERROR=%ERRORLEVEL%
