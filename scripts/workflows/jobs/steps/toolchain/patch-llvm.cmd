70590ccc4a82c66cfb8c0422c7d14864da9062c8
Alexander Smarus <bender@readdle.com>
2020-06-01 13:09:59 +0300

    [toolchain] patch llvm to be compatible with latest msvs

---
+++ scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
@@ -0,0 +1,15 @@
+setlocal
+
+pushd %CD%
+
+if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
+  cd %SW_LLVM_PROJECT_SOURCES_DIR%^
+   && git config user.name 'builder'^
+   && git config user.email 'builder@lxbndr.org'^
+   && git apply %SW_WORKSPACE%\patch\llvm-project\b288d90b39f4b905c02092a9bfcfd6d78f99b191.patch
+)
+
+set SW_ERROR=%ERRORLEVEL%
+popd
+
+endlocal & exit /b %SW_ERROR%
