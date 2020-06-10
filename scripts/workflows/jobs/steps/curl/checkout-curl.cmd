204507268d45e6dc93e136fa8934f34739d5992b
Alexander Smarus <bender@readdle.com>
2020-06-10 10:00:06 +0300

    [curl] use dev version (#5)

--- scripts/workflows/jobs/steps/curl/checkout-curl.cmd
+++ scripts/workflows/jobs/steps/curl/checkout-curl.cmd
@@ -5,10 +5,9 @@ pushd %CD%
 git init %SW_CURL_SOURCES_DIR%^
  && cd %SW_CURL_SOURCES_DIR%^
  && (git remote add origin https://github.com/curl/curl.git || ver>nul)^
- && git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_CURL_REF%*:refs/remotes/origin/%SW_CURL_REF%* +refs/tags/%SW_CURL_REF%*:refs/tags/%SW_CURL_REF%*^
+ && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_CURL_REF%*:refs/remotes/origin/%SW_CURL_REF%* +refs/tags/%SW_CURL_REF%*:refs/tags/%SW_CURL_REF%*^
  && git branch --list --remote origin/%SW_CURL_REF%^
- && git tag --list %SW_CURL_REF%^
- && git checkout --progress --force refs/tags/%SW_CURL_REF%^
+ && git checkout --progress --force -B %SW_CURL_REF% refs/remotes/origin/%SW_CURL_REF%^
  && git log -1
 
 set SW_ERROR=%ERRORLEVEL%
