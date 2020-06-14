bf17b97a28a9ac2b26387c567838ec231696766c
Alexander Smarus <bender@readdle.com>
2020-06-14 13:40:21 +0300

    [tools, icu] use set-env tool to setup icu environment

--- scripts/workflows/jobs/steps/icu/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/icu/configure-build-environment.cmd
@@ -1,16 +1,10 @@
-set SW_ICU_REF=maint/maint-%SW_ICU_VERSION%
-set SW_ICU_SOURCES_DIR=%SW_SOURCES_DIR%\icu
-set SW_ICU_BUILD_DIR=%SW_BUILD_DIR%\icu
-set SW_ICU_INSTALL_DIR=%SW_INSTALL_DIR%\Library\icu-%SW_ICU_VERSION%\usr
+call scripts\tools\set-env.cmd SW_ICU_REF maint/maint-%SW_ICU_VERSION%
+call scripts\tools\set-env.cmd SW_ICU_SOURCES_DIR %SW_SOURCES_DIR%\icu
+call scripts\tools\set-env.cmd SW_ICU_BUILD_DIR %SW_BUILD_DIR%\icu
+call scripts\tools\set-env.cmd SW_ICU_INSTALL_DIR %SW_INSTALL_DIR%\Library\icu-%SW_ICU_VERSION%\usr
 
 mkdir "%SW_ICU_SOURCES_DIR%\icu4c"
 subst T: "%SW_ICU_SOURCES_DIR%\icu4c"
 if errorlevel 1 goto :eof
 
 call scripts\tools\vs-env.cmd -arch=x64 -host_arch=x64
-if not defined GITHUB_ACTION goto :eof
-
-echo ::set-env name=SW_ICU_REF::%SW_ICU_REF%
-echo ::set-env name=SW_ICU_SOURCES_DIR::%SW_ICU_SOURCES_DIR%
-echo ::set-env name=SW_ICU_BUILD_DIR::%SW_ICU_BUILD_DIR%
-echo ::set-env name=SW_ICU_INSTALL_DIR::%SW_ICU_INSTALL_DIR%
