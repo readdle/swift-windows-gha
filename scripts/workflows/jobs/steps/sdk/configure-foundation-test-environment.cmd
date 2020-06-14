fe0ec20ecfdf4f53c1afe0f097e98c86ffa9b5ff
Alexander Smarus <bender@readdle.com>
2020-06-14 19:25:26 +0300

    [tools] use set-env script widely

--- scripts/workflows/jobs/steps/sdk/configure-foundation-test-environment.cmd
+++ scripts/workflows/jobs/steps/sdk/configure-foundation-test-environment.cmd
@@ -1,2 +1,1 @@
-set PATH=%SW_ICU_PATH%\usr\bin;%SW_STDLIB_BUILD_DIR%\bin;%SW_DISPATCH_BUILD_DIR%;%SW_FOUNDATION_BUILD_DIR%\Foundation;%SW_XCTEST_BUILD_DIR%;%PATH%;%ProgramFiles%\Git\usr\bin
-echo ::set-env name=PATH::%PATH%
+call scripts\tools\set-env.cmd PATH %SW_ICU_PATH%\usr\bin;%SW_STDLIB_BUILD_DIR%\bin;%SW_DISPATCH_BUILD_DIR%;%SW_FOUNDATION_BUILD_DIR%\Foundation;%SW_XCTEST_BUILD_DIR%;%PATH%;%ProgramFiles%\Git\usr\bin
