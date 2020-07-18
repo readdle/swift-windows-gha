ebd7ddf801e228e63166d933110dd720f6faa0c1
Alexander Smarus <bender@readdle.com>
2020-07-19 01:44:25 +0300

    [script] checkout icu using new tool

--- scripts/workflows/jobs/steps/icu/checkout-icu.cmd
+++ scripts/workflows/jobs/steps/icu/checkout-icu.cmd
@@ -1,17 +1,1 @@
-setlocal
-
-set SW_PUSHDIR=%CD%
-
-git init %SW_ICU_SOURCES_DIR%^
- && cd /d %SW_ICU_SOURCES_DIR%^
- && (git remote add origin https://github.com/unicode-org/icu || ver>nul)^
- && git config gc.auto 0^
- && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_ICU_REF%*:refs/remotes/origin/%SW_ICU_REF%* +refs/tags/%SW_ICU_REF%*:refs/tags/%SW_ICU_REF%*^
- && git branch --list --remote origin/%SW_ICU_REF%^
- && git checkout --progress --force -B %SW_ICU_REF% refs/remotes/origin/%SW_ICU_REF%^
- && git log -1
-
-set SW_ERROR=%ERRORLEVEL%
-cd /d %SW_PUSHDIR%
-
-endlocal & exit /b %SW_ERROR%
+call scripts\tools\checkout.cmd --url=https://github.com/unicode-org/icu --dir=%SW_ICU_SOURCES_DIR% --ref=%SW_ICU_REF% --depth=1
