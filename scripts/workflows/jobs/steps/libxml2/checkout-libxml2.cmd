3c92961d3f3726999a39c8ee71d44d8993844d6d
Alexander Smarus <bender@readdle.com>
2020-07-19 14:48:05 +0300

    [script] use checkout tool everywhere

--- scripts/workflows/jobs/steps/libxml2/checkout-libxml2.cmd
+++ scripts/workflows/jobs/steps/libxml2/checkout-libxml2.cmd
@@ -1,17 +1,1 @@
-setlocal
-
-set SW_PUSHDIR=%CD%
-
-git init %SW_XML2_SOURCES_DIR%^
- && cd /d %SW_XML2_SOURCES_DIR%^
- && (git remote add origin https://github.com/compnerd/libxml2.git || ver>nul)^
- && git config gc.auto 0^
- && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/%SW_XML2_REF%*:refs/remotes/origin/%SW_XML2_REF%* +refs/tags/%SW_XML2_REF%*:refs/tags/%SW_XML2_REF%*^
- && git branch --list --remote origin/%SW_XML2_REF%^
- && git checkout --progress --force -B %SW_XML2_REF% refs/remotes/origin/%SW_XML2_REF%^
- && git log -1
-
-set SW_ERROR=%ERRORLEVEL%
-cd /d %SW_PUSHDIR%
-
-endlocal & exit /b %SW_ERROR%
+call scripts\tools\checkout.cmd --url=https://github.com/compnerd/libxml2.git --dir=%SW_XML2_SOURCES_DIR% --ref=%SW_XML2_REF% --depth=1
--- scripts/workflows/jobs/steps/zlib/checkout-zlib.cmd
+++ scripts/workflows/jobs/steps/zlib/checkout-zlib.cmd
@@ -1,16 +1,1 @@
-setlocal
-
-set SW_PUSHDIR=%CD%
-
-git init %SW_ZLIB_SOURCES_DIR%^
- && cd /d %SW_ZLIB_SOURCES_DIR%^
- && (git remote add origin https://github.com/madler/zlib || ver>nul)^
- && git config gc.auto 0^
- && git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +%SW_ZLIB_REF%*:%SW_ZLIB_REF%*^
- && git checkout --progress --force %SW_ZLIB_REF%^
- && git log -1
-
-set SW_ERROR=%ERRORLEVEL%
-cd /d %SW_PUSHDIR%
-
-endlocal & exit /b %SW_ERROR%
+call scripts\tools\checkout.cmd --url=https://github.com/madler/zlib --dir=%SW_ZLIB_SOURCES_DIR% --ref=%SW_ZLIB_REF% --depth=1
