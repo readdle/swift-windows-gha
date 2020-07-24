4f62a4179121d5cdd691543fe63085da48245f4e
Alexander Smarus <bender@readdle.com>
2020-07-25 01:42:43 +0300

    [script] rework configure script to simplify parameters processing

--- scripts/configure.cmd
+++ scripts/configure.cmd
@@ -10,141 +10,29 @@ set SWW_SWIFT_BRANCH_SPEC_DEFAULT=master
 set SWW_SOURCES_DIR_DEFAULT=%CD%\w\s
 set SWW_BUILD_DIR_DEFAULT=%CD%\w\b
 set SWW_INSTALL_DIR_DEFAULT=%CD%\w\i
-set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT=N
+set SWW_OBJC_PATCH_ENABLED_DEFAULT=N
 set SWW_STDLIB_PATCH_ENABLED_DEFAULT=N
 set SWW_SWIFT_TEST_ENABLED_DEFAULT=Y
 set SWW_DISPATCH_TEST_ENABLED_DEFAULT=Y
 set SWW_FOUNDATION_TEST_ENABLED_DEFAULT=Y
 set SWW_CONFIG_FILE_DEFAULT=%CD%\config.cmd
 
-for %%x in (%*) do (
-  set SWW_CURRENT_ARG=%%~x
-  if /i [!SWW_ACTIVE_ARG!]==[--interactive] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_INTERACTIVE=!SWW_CURRENT_ARG!
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_INTERACTIVE=!SWW_CURRENT_ARG!
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--branch] (
-    if /i [!SWW_CURRENT_ARG!]==[master] (
-      set SWW_SWIFT_BRANCH_SPEC_DEFAULT=!SWW_CURRENT_ARG!
-      set SWW_BRANCH_NUM_DEFAULT=1
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[5.3] (
-      set SWW_SWIFT_BRANCH_SPEC_DEFAULT=!SWW_CURRENT_ARG!
-      set SWW_BRANCH_NUM_DEFAULT=2
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[5.2] (
-      set SWW_SWIFT_BRANCH_SPEC_DEFAULT=!SWW_CURRENT_ARG!
-      set SWW_BRANCH_NUM_DEFAULT=3
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--sources-dir] (
-    set SWW_SOURCES_DIR_DEFAULT=!SWW_CURRENT_ARG!
-    set SWW_ACTIVE_ARG=
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--build-dir] (
-    set SWW_BUILD_DIR_DEFAULT=!SWW_CURRENT_ARG!
-    set SWW_ACTIVE_ARG=
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--install-dir] (
-    set SWW_INSTALL_DIR_DEFAULT=!SWW_CURRENT_ARG!
-    set SWW_ACTIVE_ARG=
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--config] (
-    set SWW_CONFIG_FILE_DEFAULT=!SWW_CURRENT_ARG!
-    set SWW_ACTIVE_ARG=
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--test-swift] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_SWIFT_TEST_ENABLED_DEFAULT=Y
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_SWIFT_TEST_ENABLED_DEFAULT=N
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--test-dispatch] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_DISPATCH_TEST_ENABLED_DEFAULT=Y
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_DISPATCH_TEST_ENABLED_DEFAULT=N
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--test-foundation] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_FOUNDATION_TEST_ENABLED_DEFAULT=Y
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_FOUNDATION_TEST_ENABLED_DEFAULT=N
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--enable-no-objc-patch] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT=Y
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT=N
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_ACTIVE_ARG!]==[--enable-print-patch] (
-    if /i [!SWW_CURRENT_ARG!]==[YES] (
-      set SWW_STDLIB_PATCH_ENABLED_DEFAULT=Y
-      set SWW_ACTIVE_ARG=
-    ) else if /i [!SWW_CURRENT_ARG!]==[NO] (
-      set SWW_STDLIB_PATCH_ENABLED_DEFAULT=N
-      set SWW_ACTIVE_ARG=
-    ) else (
-      echo Invalid parameter value: !SWW_ACTIVE_ARG!=!SWW_CURRENT_ARG!
-      exit /b 1
-    )
-  ) else if /i [!SWW_CURRENT_ARG!]==[--interactive] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--branch] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--sources-dir] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--build-dir] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--install-dir] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--config] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--test-swift] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--test-dispatch] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--test-foundation] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--enable-no-objc-patch] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else if /i [!SWW_CURRENT_ARG!]==[--enable-print-patch] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else (
-    echo Unknown parameter: !SWW_CURRENT_ARG!
-    exit /b 1
-  )
+call :sw_parse_arguments %* && call :sw_validate_parameters
+if errorlevel 1 goto :eof
+
+if /i "%SWW_SWIFT_BRANCH_SPEC_DEFAULT%"=="master" (
+  set SWW_BRANCH_NUM_DEFAULT=1
+) else if /i "%SWW_SWIFT_BRANCH_SPEC_DEFAULT%"=="5.3" (
+  set SWW_BRANCH_NUM_DEFAULT=2
+) else if /i "%SWW_SWIFT_BRANCH_SPEC_DEFAULT%"=="5.2" (
+  set SWW_BRANCH_NUM_DEFAULT=3
 )
-if [%SWW_ACTIVE_ARG%] neq [] (
-  echo Parameter value not specified: !SWW_ACTIVE_ARG!
-  exit /b 1
-)
+
+call :sw_normalize_bool_parameter_for_wizard SWW_SWIFT_TEST_ENABLED_DEFAULT
+call :sw_normalize_bool_parameter_for_wizard SWW_DISPATCH_TEST_ENABLED_DEFAULT
+call :sw_normalize_bool_parameter_for_wizard SWW_FOUNDATION_TEST_ENABLED_DEFAULT
+call :sw_normalize_bool_parameter_for_wizard SWW_OBJC_PATCH_ENABLED_DEFAULT
+call :sw_normalize_bool_parameter_for_wizard SWW_STDLIB_PATCH_ENABLED_DEFAULT
 
 if /i [%SWW_INTERACTIVE%]==[NO] (
   set SWW_SWIFT_BRANCH_SPEC=%SWW_SWIFT_BRANCH_SPEC_DEFAULT%
@@ -155,23 +43,14 @@ if /i [%SWW_INTERACTIVE%]==[NO] (
   set SWW_DISPATCH_TEST_ENABLED=%SWW_DISPATCH_TEST_ENABLED_DEFAULT%
   set SWW_FOUNDATION_TEST_ENABLED=%SWW_FOUNDATION_TEST_ENABLED_DEFAULT%
   set SWW_CONFIG_FILE=%SWW_CONFIG_FILE_DEFAULT%
-  set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT%
+  set SWW_OBJC_PATCH_ENABLED=%SWW_OBJC_PATCH_ENABLED_DEFAULT%
   set SWW_STDLIB_PATCH_ENABLED=%SWW_STDLIB_PATCH_ENABLED_DEFAULT%
   goto configure
 )
 
 :wizard-start
 
-if [%SWW_STDLIB_PATCH_ENABLED%]==[YES] set SWW_STDLIB_PATCH_ENABLED=Y
-if [%SWW_STDLIB_PATCH_ENABLED%]==[NO] set SWW_STDLIB_PATCH_ENABLED=N
-if [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[YES] set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=Y
-if [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[NO] set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=N
-if [%SWW_SWIFT_TEST_ENABLED%]==[YES] set SWW_SWIFT_TEST_ENABLED=Y
-if [%SWW_SWIFT_TEST_ENABLED%]==[NO] set SWW_SWIFT_TEST_ENABLED=N
-if [%SWW_DISPATCH_TEST_ENABLED%]==[YES] set SWW_DISPATCH_TEST_ENABLED=Y
-if [%SWW_DISPATCH_TEST_ENABLED%]==[NO] set SWW_DISPATCH_TEST_ENABLED=N
-if [%SWW_FOUNDATION_TEST_ENABLED%]==[YES] set SWW_FOUNDATION_TEST_ENABLED=Y
-if [%SWW_FOUNDATION_TEST_ENABLED%]==[NO] set SWW_FOUNDATION_TEST_ENABLED=N
+call :sw_normalize_parameters_for_wizard
 
 :ask-branch
 
@@ -210,12 +89,12 @@ set /p SWW_INSTALL_DIR="Enter directory to install into (%SWW_INSTALL_DIR_DEFAUL
 set SWW_INSTALL_DIR_DEFAULT=%SWW_INSTALL_DIR%
 
 :ask-swift-patch
-set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT%
-set /p SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED="Apply @objc patch (%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT%)?: "
-if /i not [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[Y] if /i not [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[N] (
+set SWW_OBJC_PATCH_ENABLED=%SWW_OBJC_PATCH_ENABLED_DEFAULT%
+set /p SWW_OBJC_PATCH_ENABLED="Apply @objc patch (%SWW_OBJC_PATCH_ENABLED_DEFAULT%)?: "
+if /i not [%SWW_OBJC_PATCH_ENABLED%]==[Y] if /i not [%SWW_OBJC_PATCH_ENABLED%]==[N] (
   goto ask-swift-patch  
 )
-set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED_DEFAULT=%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%
+set SWW_OBJC_PATCH_ENABLED_DEFAULT=%SWW_OBJC_PATCH_ENABLED%
 
 :ask-stdlib-patch
 set SWW_STDLIB_PATCH_ENABLED=%SWW_STDLIB_PATCH_ENABLED_DEFAULT%
@@ -270,16 +149,7 @@ set SWW_CURL_VERSION=development
 set SWW_XML2_VERSION=development
 set SWW_ZLIB_VERSION=1.2.11
 
-if /i [%SWW_STDLIB_PATCH_ENABLED%]==[Y] set SWW_STDLIB_PATCH_ENABLED=YES
-if /i [%SWW_STDLIB_PATCH_ENABLED%]==[N] set SWW_STDLIB_PATCH_ENABLED=NO
-if /i [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[Y] set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=YES
-if /i [%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%]==[N] set SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED=NO
-if /i [%SWW_SWIFT_TEST_ENABLED%]==[Y] set SWW_SWIFT_TEST_ENABLED=YES
-if /i [%SWW_SWIFT_TEST_ENABLED%]==[N] set SWW_SWIFT_TEST_ENABLED=NO
-if /i [%SWW_DISPATCH_TEST_ENABLED%]==[Y] set SWW_DISPATCH_TEST_ENABLED=YES
-if /i [%SWW_DISPATCH_TEST_ENABLED%]==[N] set SWW_DISPATCH_TEST_ENABLED=NO
-if /i [%SWW_FOUNDATION_TEST_ENABLED%]==[Y] set SWW_FOUNDATION_TEST_ENABLED=YES
-if /i [%SWW_FOUNDATION_TEST_ENABLED%]==[N] set SWW_FOUNDATION_TEST_ENABLED=NO
+call :sw_normalize_parameters_for_saving
 
 echo.
 echo Swift branch spec:       %SWW_SWIFT_BRANCH_SPEC%
@@ -292,7 +162,7 @@ echo ICU version:             %SWW_ICU_VERSION%
 echo XML2 version:            %SWW_XML2_VERSION%
 echo ZLIB version:            %SWW_ZLIB_VERSION%
 echo.
-echo Swift @objc patch:       %SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%
+echo Swift @objc patch:       %SWW_OBJC_PATCH_ENABLED%
 echo Swift print patch:       %SWW_STDLIB_PATCH_ENABLED%
 echo.
 echo Swift test enabled:      %SWW_SWIFT_TEST_ENABLED%
@@ -327,7 +197,7 @@ echo set SW_ICU_VERSION=%SWW_ICU_VERSION%>>%SWW_CONFIG_FILE%
 echo set SW_XML2_VERSION=%SWW_XML2_VERSION%>>%SWW_CONFIG_FILE%
 echo set SW_ZLIB_VERSION=%SWW_ZLIB_VERSION%>>%SWW_CONFIG_FILE%
 echo.>>%SWW_CONFIG_FILE%
-echo set SW_EXPERIMENTAL_OBJC_PATCH_ENABLED=%SWW_EXPERIMENTAL_OBJC_PATCH_ENABLED%>>%SWW_CONFIG_FILE%
+echo set SW_EXPERIMENTAL_OBJC_PATCH_ENABLED=%SWW_OBJC_PATCH_ENABLED%>>%SWW_CONFIG_FILE%
 echo set SW_STDLIB_PATCH_ENABLED=%SWW_STDLIB_PATCH_ENABLED%>>%SWW_CONFIG_FILE%
 echo.>>%SWW_CONFIG_FILE%
 echo set SW_SWIFT_TEST_ENABLED=%SWW_SWIFT_TEST_ENABLED%>>%SWW_CONFIG_FILE%
@@ -335,5 +205,185 @@ echo set SW_DISPATCH_TEST_ENABLED=%SWW_DISPATCH_TEST_ENABLED%>>%SWW_CONFIG_FILE%
 echo set SW_FOUNDATION_TEST_ENABLED=%SWW_FOUNDATION_TEST_ENABLED%>>%SWW_CONFIG_FILE%
 
 echo Configuration saved. Run build.cmd.
+
+goto :eof
+endlocal
+
+
+
+rem ###########################################################################
+:sw_parse_arguments <arguments...>
+:sw_parse_arguments_start
+set CURRENT_ARG=%~1
+
+if "%NEXT_ARG%"=="SWW_INTERACTIVE"                      goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_SWIFT_BRANCH_SPEC_DEFAULT"        goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_SOURCES_DIR_DEFAULT"              goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_BUILD_DIR_DEFAULT"                goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_INSTALL_DIR_DEFAULT"              goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_CONFIG_FILE_DEFAULT"              goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_SWIFT_TEST_ENABLED_DEFAULT"       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_DISPATCH_TEST_ENABLED_DEFAULT"    goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_FOUNDATION_TEST_ENABLED_DEFAULT"  goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_OBJC_PATCH_ENABLED_DEFAULT"       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SWW_STDLIB_PATCH_ENABLED_DEFAULT"     goto sw_parse_arguments_accept
+
+if not defined CURRENT_ARG goto sw_parse_argumens_end
+
+if "%CURRENT_ARG%"=="--interactive" (                   set NEXT_ARG=SWW_INTERACTIVE
+) else if "%CURRENT_ARG%"=="--branch" (                 set NEXT_ARG=SWW_SWIFT_BRANCH_SPEC_DEFAULT
+) else if "%CURRENT_ARG%"=="--sources-dir" (            set NEXT_ARG=SWW_SOURCES_DIR_DEFAULT
+) else if "%CURRENT_ARG%"=="--build-dir" (              set NEXT_ARG=SWW_BUILD_DIR_DEFAULT
+) else if "%CURRENT_ARG%"=="--install-dir" (            set NEXT_ARG=SWW_INSTALL_DIR_DEFAULT
+) else if "%CURRENT_ARG%"=="--config" (                 set NEXT_ARG=SWW_CONFIG_FILE_DEFAULT
+) else if "%CURRENT_ARG%"=="--test-swift" (             set NEXT_ARG=SWW_SWIFT_TEST_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--test-dispatch" (          set NEXT_ARG=SWW_DISPATCH_TEST_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--test-foundation" (        set NEXT_ARG=SWW_FOUNDATION_TEST_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--enable-no-objc-patch" (   set NEXT_ARG=SWW_OBJC_PATCH_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--enable-print-patch" (     set NEXT_ARG=SWW_STDLIB_PATCH_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--help" (                   goto help
+) else (
+  echo Unknown argument: %CURRENT_ARG%
+  exit /b 1
+)
+goto :sw_parse_arguments_next
+
+:sw_parse_arguments_accept
+set "%NEXT_ARG%=%CURRENT_ARG%"
+set NEXT_ARG=
+
+:sw_parse_arguments_next
+shift
+goto sw_parse_arguments_start
+:sw_parse_argumens_end
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_validate_parameters
+setlocal enabledelayedexpansion
+for %%G in (SWW_INTERACTIVE SWW_SWIFT_BRANCH_SPEC_DEFAULT SWW_SOURCES_DIR_DEFAULT SWW_BUILD_DIR_DEFAULT SWW_INSTALL_DIR_DEFAULT SWW_CONFIG_FILE_DEFAULT SWW_SWIFT_TEST_ENABLED_DEFAULT SWW_DISPATCH_TEST_ENABLED_DEFAULT SWW_FOUNDATION_TEST_ENABLED_DEFAULT SWW_OBJC_PATCH_ENABLED_DEFAULT SWW_STDLIB_PATCH_ENABLED_DEFAULT) do (
+  call :sw_validate_parameter %%G
+  if errorlevel 1 goto sw_validate_parameters_fail
+)
+endlocal
+exit /b
+
+:sw_validate_parameters_fail
+endlocal
+exit /b 1
+
+
+rem ###########################################################################
+:sw_validate_parameter <parameter>
+setlocal enabledelayedexpansion
+
+set PARAMETER=%1
+set VALUE=!%PARAMETER%!
+
+if "%PARAMETER%"=="SWW_INTERACTIVE" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_SWIFT_BRANCH_SPEC_DEFAULT" (
+  if /i not "%VALUE%"=="master" if /i not "%VALUE%"=="5.3" if /i not "%VALUE%"=="5.2" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_SOURCES_DIR_DEFAULT" (
+  if "%VALUE%"=="" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_BUILD_DIR_DEFAULT" (
+  if "%VALUE%"=="" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_INSTALL_DIR_DEFAULT" (
+  if "%VALUE%"=="" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_CONFIG_FILE_DEFAULT" (
+  if "%VALUE%"=="" goto :sw_validate_parameter_fail
+) else if "%PARAMETER%"=="SWW_SWIFT_TEST_ENABLED_DEFAULT" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
+    goto :sw_validate_parameter_fail
+  )
+) else if "%PARAMETER%"=="SWW_DISPATCH_TEST_ENABLED_DEFAULT" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
+    goto :sw_validate_parameter_fail
+  )
+) else if "%PARAMETER%"=="SWW_FOUNDATION_TEST_ENABLED_DEFAULT" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
+    goto :sw_validate_parameter_fail
+  )
+) else if "%PARAMETER%"=="SWW_OBJC_PATCH_ENABLED_DEFAULT" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
+    goto :sw_validate_parameter_fail
+  )
+) else if "%PARAMETER%"=="SWW_STDLIB_PATCH_ENABLED_DEFAULT" (
+  if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
+    goto :sw_validate_parameter_fail
+  )
+)
+endlocal
+exit /b
+
+:sw_validate_parameter_fail
+echo %PARAMETER% - Invalid value: %VALUE%
+endlocal
+exit /b 1
+
+
+
+rem ###########################################################################
+:sw_normalize_parameters_for_wizard
+
+call :sw_normalize_bool_parameter_for_wizard SWW_STDLIB_PATCH_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SWW_OBJC_PATCH_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SWW_SWIFT_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SWW_DISPATCH_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SWW_FOUNDATION_TEST_ENABLED
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_normalize_parameters_for_saving
+
+call :sw_normalize_bool_parameter_for_saving SWW_STDLIB_PATCH_ENABLED
+call :sw_normalize_bool_parameter_for_saving SWW_OBJC_PATCH_ENABLED
+call :sw_normalize_bool_parameter_for_saving SWW_SWIFT_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_saving SWW_DISPATCH_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_saving SWW_FOUNDATION_TEST_ENABLED
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_normalize_bool_parameter_for_wizard <parameter>
+setlocal enabledelayedexpansion
+
+set PARAMETER=%1
+set VALUE=!%PARAMETER%!
+
+if "%VALUE%"=="YES" set VALUE=Y
+if "%VALUE%"=="NO" set VALUE=N
+
+(
+  endlocal
+  set %1=%VALUE%
+  exit /b
+)
+
+
+
+rem ###########################################################################
+:sw_normalize_bool_parameter_for_saving <parameter>
+setlocal enabledelayedexpansion
+
+set PARAMETER=%1
+set VALUE=!%PARAMETER%!
+
+if "%VALUE%"=="Y" set VALUE=YES
+if "%VALUE%"=="N" set VALUE=NO
+
+(
+  endlocal
+  set %1=%VALUE%
+  exit /b
+)
 
 endlocal
