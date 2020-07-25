bc87532ddac0d32ac925f4230cd17f05042e6952
Alexander Smarus <bender@readdle.com>
2020-07-26 00:34:32 +0300

    [script] refactor arguments parsing and validation

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -4,27 +4,8 @@ setlocal enabledelayedexpansion
 
 set SW_CONFIG_FILE=config.cmd
 
-for %%x in (%*) do (
-  set SWW_CURRENT_ARG=%%~x
-  if /i [!SWW_ACTIVE_ARG!]==[--config] (
-    set SW_CONFIG_FILE=!SWW_CURRENT_ARG!
-    set SWW_ACTIVE_ARG=
-  ) else if /i [!SWW_CURRENT_ARG!]==[--config] (
-    set SWW_ACTIVE_ARG=!SWW_CURRENT_ARG!
-  ) else (
-    echo Unknown parameter: !SWW_CURRENT_ARG!
-    exit /b 1
-  )
-)
-if [%SWW_ACTIVE_ARG%] neq [] (
-  echo Parameter value not specified: !SWW_ACTIVE_ARG!
-  exit /b 1
-)
-
-if not exist %SW_CONFIG_FILE% (
-    echo Configuration not found: %SW_CONFIG_FILE%
-    exit /b 1
-)
+call :sw_parse_arguments %* && call :sw_validate_parameters
+if errorlevel 1 goto :eof
 
 call scripts\tools\vs-env.cmd -arch=x64 -host_arch=x64
 
@@ -33,6 +14,7 @@ call %SW_CONFIG_FILE%
 
 echo.
 echo Swift branch spec:       %SW_SWIFT_BRANCH_SPEC%
+echo Swift SDK spec:          %SW_SWIFT_SDK_SPEC%
 echo Source files directory:  %SW_SOURCES_DIR%
 echo Build output directory:  %SW_BUILD_DIR%
 echo Install directory:       %SW_INSTALL_DIR%
@@ -63,3 +45,73 @@ call %SW_JOBS_DIR%\icu.cmd^
  && call %SW_JOBS_DIR%\sdk.cmd
  
 endlocal
+goto :eof
+
+
+
+rem ###########################################################################
+:sw_parse_arguments <arguments...>
+:sw_parse_arguments_start
+set CURRENT_ARG=%~1
+
+if "%NEXT_ARG%"=="SW_CONFIG_FILE" goto sw_parse_arguments_accept
+
+if not defined CURRENT_ARG goto sw_parse_argumens_end
+
+if "%CURRENT_ARG%"=="--config" (
+  set NEXT_ARG=SW_CONFIG_FILE
+) else (
+  echo Unknown argument: %CURRENT_ARG%
+  exit /b 1
+)
+goto :sw_parse_arguments_next
+
+:sw_parse_arguments_accept
+set "%NEXT_ARG%=%CURRENT_ARG%"
+set NEXT_ARG=
+
+:sw_parse_arguments_next
+shift
+goto sw_parse_arguments_start
+:sw_parse_argumens_end
+
+exit /b
+
+
+rem ###########################################################################
+:sw_validate_parameters
+setlocal enabledelayedexpansion
+for %%G in (SW_CONFIG_FILE) do (
+  call :sw_validate_parameter %%G
+  if errorlevel 1 goto sw_validate_parameters_fail
+)
+endlocal
+exit /b
+
+:sw_validate_parameters_fail
+endlocal
+exit /b 1
+
+
+rem ###########################################################################
+:sw_validate_parameter <parameter>
+setlocal enabledelayedexpansion
+
+set PARAMETER=%1
+set VALUE=!%PARAMETER%!
+
+if "%PARAMETER%"=="SW_CONFIG_FILE" (
+  if "%VALUE%"=="" goto :sw_validate_parameter_fail
+  if not exist "%VALUE%" (
+    echo Configuration not found: %VALUE%
+    goto :sw_validate_parameter_fail
+  )
+)
+
+endlocal
+exit /b
+
+:sw_validate_parameter_fail
+echo %PARAMETER% - Invalid value: %VALUE%
+endlocal
+exit /b 1
