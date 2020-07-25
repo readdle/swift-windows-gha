e2afde2c51cec2431a789f5d712d93f363aeb5ae
Alexander Smarus <bender@readdle.com>
2020-07-25 19:14:28 +0300

    [script] simplify parameters processing in config script

--- scripts/configure.cmd
+++ scripts/configure.cmd
@@ -4,138 +4,50 @@ setlocal enabledelayedexpansion
 
 set SW_INTERACTIVE=YES
 
-set SW_BRANCH_NUM_DEFAULT=1
-set SW_SWIFT_BRANCH_SPEC_DEFAULT=master
-set SW_SOURCES_DIR_DEFAULT=%CD%\w\s
-set SW_BUILD_DIR_DEFAULT=%CD%\w\b
-set SW_INSTALL_DIR_DEFAULT=%CD%\w\i
-set SW_OBJC_PATCH_ENABLED_DEFAULT=NO
-set SW_STDLIB_PATCH_ENABLED_DEFAULT=NO
-set SW_SWIFT_TEST_ENABLED_DEFAULT=YES
-set SW_DISPATCH_TEST_ENABLED_DEFAULT=YES
-set SW_FOUNDATION_TEST_ENABLED_DEFAULT=YES
-set SW_CONFIG_FILE_DEFAULT=%CD%\config.cmd
+set SW_SWIFT_BRANCH_SPEC=master
+set SW_SOURCES_DIR=%CD%\w\s
+set SW_BUILD_DIR=%CD%\w\b
+set SW_INSTALL_DIR=%CD%\w\i
+set SW_OBJC_PATCH_ENABLED=NO
+set SW_STDLIB_PATCH_ENABLED=NO
+set SW_SWIFT_TEST_ENABLED=YES
+set SW_DISPATCH_TEST_ENABLED=YES
+set SW_FOUNDATION_TEST_ENABLED=YES
+set SW_CONFIG_FILE=%CD%\config.cmd
 
 call :sw_parse_arguments %* && call :sw_validate_parameters
 if errorlevel 1 goto :eof
 
-if /i "%SW_SWIFT_BRANCH_SPEC_DEFAULT%"=="master" (
-  set SW_BRANCH_NUM_DEFAULT=1
-) else if /i "%SW_SWIFT_BRANCH_SPEC_DEFAULT%"=="5.3" (
-  set SW_BRANCH_NUM_DEFAULT=2
-) else if /i "%SW_SWIFT_BRANCH_SPEC_DEFAULT%"=="5.2" (
-  set SW_BRANCH_NUM_DEFAULT=3
+if /i "%SW_SWIFT_BRANCH_SPEC%"=="master" (
+  set SW_BRANCH_NUM=1
+) else if /i "%SW_SWIFT_BRANCH_SPEC%"=="5.3" (
+  set SW_BRANCH_NUM=2
+) else if /i "%SW_SWIFT_BRANCH_SPEC%"=="5.2" (
+  set SW_BRANCH_NUM=3
 )
 
-call :sw_normalize_bool_parameter_for_wizard SW_SWIFT_TEST_ENABLED_DEFAULT
-call :sw_normalize_bool_parameter_for_wizard SW_DISPATCH_TEST_ENABLED_DEFAULT
-call :sw_normalize_bool_parameter_for_wizard SW_FOUNDATION_TEST_ENABLED_DEFAULT
-call :sw_normalize_bool_parameter_for_wizard SW_OBJC_PATCH_ENABLED_DEFAULT
-call :sw_normalize_bool_parameter_for_wizard SW_STDLIB_PATCH_ENABLED_DEFAULT
+call :sw_normalize_bool_parameter_for_wizard SW_OBJC_PATCH_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SW_SWIFT_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SW_DISPATCH_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SW_FOUNDATION_TEST_ENABLED
+call :sw_normalize_bool_parameter_for_wizard SW_STDLIB_PATCH_ENABLED
 
 if /i [%SW_INTERACTIVE%]==[NO] (
-  set SW_SWIFT_BRANCH_SPEC=%SW_SWIFT_BRANCH_SPEC_DEFAULT%
-  set SW_SOURCES_DIR=%SW_SOURCES_DIR_DEFAULT%
-  set SW_BUILD_DIR=%SW_BUILD_DIR_DEFAULT%
-  set SW_INSTALL_DIR=%SW_INSTALL_DIR_DEFAULT%
-  set SW_SWIFT_TEST_ENABLED=%SW_SWIFT_TEST_ENABLED_DEFAULT%
-  set SW_DISPATCH_TEST_ENABLED=%SW_DISPATCH_TEST_ENABLED_DEFAULT%
-  set SW_FOUNDATION_TEST_ENABLED=%SW_FOUNDATION_TEST_ENABLED_DEFAULT%
-  set SW_CONFIG_FILE=%SW_CONFIG_FILE_DEFAULT%
-  set SW_OBJC_PATCH_ENABLED=%SW_OBJC_PATCH_ENABLED_DEFAULT%
-  set SW_STDLIB_PATCH_ENABLED=%SW_STDLIB_PATCH_ENABLED_DEFAULT%
   goto configure
 )
 
-:wizard-start
+:sw_wizard_start
 
 call :sw_normalize_parameters_for_wizard
 
-:ask-branch
-
-echo Available branches:
-echo.  1. master
-echo.  2. 5.3
-echo.  3. 5.2
-
-set SW_BRANCH_NUM=%SW_BRANCH_NUM_DEFAULT%
-set /p SW_BRANCH_NUM="Enter branch number to build (%SW_BRANCH_NUM_DEFAULT%): "
-
-if [%SW_BRANCH_NUM%]==[1] (
-  set SW_SWIFT_BRANCH_SPEC=master
-) else if [%SW_BRANCH_NUM%]==[2] (
-  set SW_SWIFT_BRANCH_SPEC=5.3
-) else if [%SW_BRANCH_NUM%]==[3] (
-  set SW_SWIFT_BRANCH_SPEC=5.2
-) else (
-  goto ask-branch
-)
-set SW_BRANCH_NUM_DEFAULT=%SW_BRANCH_NUM%
-
-:ask-src-dir
-set SW_SOURCES_DIR=%SW_SOURCES_DIR_DEFAULT%
-set /p SW_SOURCES_DIR="Enter directory for source files (%SW_SOURCES_DIR_DEFAULT%): "
-set SW_SOURCES_DIR_DEFAULT=%SW_SOURCES_DIR%
-
-:ask-build-dir
-set SW_BUILD_DIR=%SW_BUILD_DIR_DEFAULT%
-set /p SW_BUILD_DIR="Enter directory for build output (%SW_BUILD_DIR_DEFAULT%): "
-set SW_BUILD_DIR_DEFAULT=%SW_BUILD_DIR%
-
-:ask-install-dir
-set SW_INSTALL_DIR=%SW_INSTALL_DIR_DEFAULT%
-set /p SW_INSTALL_DIR="Enter directory to install into (%SW_INSTALL_DIR_DEFAULT%): "
-set SW_INSTALL_DIR_DEFAULT=%SW_INSTALL_DIR%
-
-:ask-swift-patch
-set SW_OBJC_PATCH_ENABLED=%SW_OBJC_PATCH_ENABLED_DEFAULT%
-set /p SW_OBJC_PATCH_ENABLED="Apply @objc patch (%SW_OBJC_PATCH_ENABLED_DEFAULT%)?: "
-if /i not [%SW_OBJC_PATCH_ENABLED%]==[Y] if /i not [%SW_OBJC_PATCH_ENABLED%]==[N] (
-  goto ask-swift-patch  
-)
-set SW_OBJC_PATCH_ENABLED_DEFAULT=%SW_OBJC_PATCH_ENABLED%
-
-:ask-stdlib-patch
-set SW_STDLIB_PATCH_ENABLED=%SW_STDLIB_PATCH_ENABLED_DEFAULT%
-set /p SW_STDLIB_PATCH_ENABLED="Apply print flush patch (%SW_STDLIB_PATCH_ENABLED_DEFAULT%)?: "
-if /i not [%SW_STDLIB_PATCH_ENABLED%]==[Y] if /i not [%SW_STDLIB_PATCH_ENABLED%]==[N] (
-  goto ask-stdlib-patch  
-)
-set SW_STDLIB_PATCH_ENABLED_DEFAULT=%SW_STDLIB_PATCH_ENABLED%
-
-:ask-swift-test
-set SW_SWIFT_TEST_ENABLED=%SW_SWIFT_TEST_ENABLED_DEFAULT%
-set /p SW_SWIFT_TEST_ENABLED="Enable Swift test (%SW_SWIFT_TEST_ENABLED_DEFAULT%)?: "
-if /i not [%SW_SWIFT_TEST_ENABLED%]==[Y] if /i not [%SW_SWIFT_TEST_ENABLED%]==[N] (
-  goto ask-swift-test  
-)
-set SW_SWIFT_TEST_ENABLED_DEFAULT=%SW_SWIFT_TEST_ENABLED%
-
-:ask-dispatch-test
-set SW_DISPATCH_TEST_ENABLED=%SW_DISPATCH_TEST_ENABLED_DEFAULT%
-set /p SW_DISPATCH_TEST_ENABLED="Enable Dispatch test (%SW_DISPATCH_TEST_ENABLED_DEFAULT%)?: "
-if /i not [%SW_DISPATCH_TEST_ENABLED%]==[Y] if /i not [%SW_DISPATCH_TEST_ENABLED%]==[N] (
-  goto ask-dispatch-test  
-)
-set SW_DISPATCH_TEST_ENABLED_DEFAULT=%SW_DISPATCH_TEST_ENABLED%
-
-:ask-foundation-test
-set SW_FOUNDATION_TEST_ENABLED=%SW_FOUNDATION_TEST_ENABLED_DEFAULT%
-set /p SW_FOUNDATION_TEST_ENABLED="Enable Foundation test (%SW_FOUNDATION_TEST_ENABLED_DEFAULT%)?: "
-if [%SW_FOUNDATION_TEST_ENABLED%]==[] (
-  set SW_FOUNDATION_TEST_ENABLED=%SW_FOUNDATION_TEST_ENABLED_DEFAULT%
-)
-if /i not [%SW_FOUNDATION_TEST_ENABLED%]==[Y] if /i not [%SW_FOUNDATION_TEST_ENABLED%]==[N] (
-  goto ask-foundation-test  
-)
-set SW_FOUNDATION_TEST_ENABLED_DEFAULT=%SW_FOUNDATION_TEST_ENABLED%
-
-:ask-config-file
-set /p SW_CONFIG_FILE="Enter configuration file name to save (%SW_CONFIG_FILE_DEFAULT%): "
-if [%SW_CONFIG_FILE%]==[] (
-  set SW_CONFIG_FILE=%SW_CONFIG_FILE_DEFAULT%
-)
-set SW_CONFIG_FILE_DEFAULT=%SW_CONFIG_FILE%
+call :sw_ask_branch
+call :sw_ask_directories
+call :sw_ask_swift_patch
+call :sw_ask_stdlib_patch
+call :sw_ask_swift_test
+call :sw_ask_dispatch_test
+call :sw_ask_foundation_test
+call :sw_ask_config_file
 
 :configure
 if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
@@ -171,22 +83,20 @@ echo Foundation test enabled: %SW_FOUNDATION_TEST_ENABLED%
 echo Configuration file:      %SW_CONFIG_FILE%
 echo.
 
-if /i [%SW_INTERACTIVE%]==[NO] goto save-config
+if /i [%SW_INTERACTIVE%]==[NO] goto sw_save_config
 
-:ask-save
+:sw_ask_save
 set SW_SAVE_CONFIG=Y
-set /p SW_SAVE_CONFIG="Do you want to save this configuration? (Y/n): "
+set /p SW_SAVE_CONFIG="Do you want to save this configuration? (%SW_SAVE_CONFIG%): "
 call :sw_normalize_bool_input SW_SAVE_CONFIG
-if "%SW_SAVE_CONFIG%"=="Y" (
-  goto save-config
-) else if "%SW_SAVE_CONFIG%"=="N" (
-  echo.
-  goto wizard-start
-) else (
-  goto ask-save
-)
+call :sw_validate_bool_input SW_SAVE_CONFIG
+if errorlevel 1 goto sw_ask_save
 
-:save-config
+if "%SW_SAVE_CONFIG%"=="Y" goto sw_save_config
+if "%SW_SAVE_CONFIG%"=="N" goto sw_wizard_start
+
+
+:sw_save_config
 echo set SW_SWIFT_BRANCH_SPEC=%SW_SWIFT_BRANCH_SPEC%>%SW_CONFIG_FILE%
 echo set SW_SOURCES_DIR=%SW_SOURCES_DIR%>>%SW_CONFIG_FILE%
 echo set SW_BUILD_DIR=%SW_BUILD_DIR%>>%SW_CONFIG_FILE%
@@ -216,31 +126,31 @@ rem ###########################################################################
 :sw_parse_arguments_start
 set CURRENT_ARG=%~1
 
-if "%NEXT_ARG%"=="SW_INTERACTIVE"                      goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_SWIFT_BRANCH_SPEC_DEFAULT"        goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_SOURCES_DIR_DEFAULT"              goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_BUILD_DIR_DEFAULT"                goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_INSTALL_DIR_DEFAULT"              goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_CONFIG_FILE_DEFAULT"              goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_SWIFT_TEST_ENABLED_DEFAULT"       goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_DISPATCH_TEST_ENABLED_DEFAULT"    goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_FOUNDATION_TEST_ENABLED_DEFAULT"  goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_OBJC_PATCH_ENABLED_DEFAULT"       goto sw_parse_arguments_accept
-if "%NEXT_ARG%"=="SW_STDLIB_PATCH_ENABLED_DEFAULT"     goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_INTERACTIVE"                       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_SWIFT_BRANCH_SPEC"                 goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_SOURCES_DIR"                       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_BUILD_DIR"                         goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_INSTALL_DIR"                       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_CONFIG_FILE"                       goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_SWIFT_TEST_ENABLED"                goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_DISPATCH_TEST_ENABLED"             goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_FOUNDATION_TEST_ENABLED"           goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_OBJC_PATCH_ENABLED"                goto sw_parse_arguments_accept
+if "%NEXT_ARG%"=="SW_STDLIB_PATCH_ENABLED"              goto sw_parse_arguments_accept
 
 if not defined CURRENT_ARG goto sw_parse_argumens_end
 
 if "%CURRENT_ARG%"=="--interactive" (                   set NEXT_ARG=SW_INTERACTIVE
-) else if "%CURRENT_ARG%"=="--branch" (                 set NEXT_ARG=SW_SWIFT_BRANCH_SPEC_DEFAULT
-) else if "%CURRENT_ARG%"=="--sources-dir" (            set NEXT_ARG=SW_SOURCES_DIR_DEFAULT
-) else if "%CURRENT_ARG%"=="--build-dir" (              set NEXT_ARG=SW_BUILD_DIR_DEFAULT
-) else if "%CURRENT_ARG%"=="--install-dir" (            set NEXT_ARG=SW_INSTALL_DIR_DEFAULT
-) else if "%CURRENT_ARG%"=="--config" (                 set NEXT_ARG=SW_CONFIG_FILE_DEFAULT
-) else if "%CURRENT_ARG%"=="--test-swift" (             set NEXT_ARG=SW_SWIFT_TEST_ENABLED_DEFAULT
-) else if "%CURRENT_ARG%"=="--test-dispatch" (          set NEXT_ARG=SW_DISPATCH_TEST_ENABLED_DEFAULT
-) else if "%CURRENT_ARG%"=="--test-foundation" (        set NEXT_ARG=SW_FOUNDATION_TEST_ENABLED_DEFAULT
-) else if "%CURRENT_ARG%"=="--enable-no-objc-patch" (   set NEXT_ARG=SW_OBJC_PATCH_ENABLED_DEFAULT
-) else if "%CURRENT_ARG%"=="--enable-print-patch" (     set NEXT_ARG=SW_STDLIB_PATCH_ENABLED_DEFAULT
+) else if "%CURRENT_ARG%"=="--branch" (                 set NEXT_ARG=SW_SWIFT_BRANCH_SPEC
+) else if "%CURRENT_ARG%"=="--sources-dir" (            set NEXT_ARG=SW_SOURCES_DIR
+) else if "%CURRENT_ARG%"=="--build-dir" (              set NEXT_ARG=SW_BUILD_DIR
+) else if "%CURRENT_ARG%"=="--install-dir" (            set NEXT_ARG=SW_INSTALL_DIR
+) else if "%CURRENT_ARG%"=="--config" (                 set NEXT_ARG=SW_CONFIG_FILE
+) else if "%CURRENT_ARG%"=="--test-swift" (             set NEXT_ARG=SW_SWIFT_TEST_ENABLED
+) else if "%CURRENT_ARG%"=="--test-dispatch" (          set NEXT_ARG=SW_DISPATCH_TEST_ENABLED
+) else if "%CURRENT_ARG%"=="--test-foundation" (        set NEXT_ARG=SW_FOUNDATION_TEST_ENABLED
+) else if "%CURRENT_ARG%"=="--enable-no-objc-patch" (   set NEXT_ARG=SW_OBJC_PATCH_ENABLED
+) else if "%CURRENT_ARG%"=="--enable-print-patch" (     set NEXT_ARG=SW_STDLIB_PATCH_ENABLED
 ) else if "%CURRENT_ARG%"=="--help" (                   goto help
 ) else (
   echo Unknown argument: %CURRENT_ARG%
@@ -264,7 +174,7 @@ exit /b
 rem ###########################################################################
 :sw_validate_parameters
 setlocal enabledelayedexpansion
-for %%G in (SW_INTERACTIVE SW_SWIFT_BRANCH_SPEC_DEFAULT SW_SOURCES_DIR_DEFAULT SW_BUILD_DIR_DEFAULT SW_INSTALL_DIR_DEFAULT SW_CONFIG_FILE_DEFAULT SW_SWIFT_TEST_ENABLED_DEFAULT SW_DISPATCH_TEST_ENABLED_DEFAULT SW_FOUNDATION_TEST_ENABLED_DEFAULT SW_OBJC_PATCH_ENABLED_DEFAULT SW_STDLIB_PATCH_ENABLED_DEFAULT) do (
+for %%G in (SW_INTERACTIVE SW_SWIFT_BRANCH_SPEC SW_SOURCES_DIR SW_BUILD_DIR SW_INSTALL_DIR SW_CONFIG_FILE SW_SWIFT_TEST_ENABLED SW_DISPATCH_TEST_ENABLED SW_FOUNDATION_TEST_ENABLED SW_OBJC_PATCH_ENABLED SW_STDLIB_PATCH_ENABLED) do (
   call :sw_validate_parameter %%G
   if errorlevel 1 goto sw_validate_parameters_fail
 )
@@ -285,33 +195,33 @@ set VALUE=!%PARAMETER%!
 
 if "%PARAMETER%"=="SW_INTERACTIVE" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_SWIFT_BRANCH_SPEC_DEFAULT" (
+) else if "%PARAMETER%"=="SW_SWIFT_BRANCH_SPEC" (
   if /i not "%VALUE%"=="master" if /i not "%VALUE%"=="5.3" if /i not "%VALUE%"=="5.2" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_SOURCES_DIR_DEFAULT" (
+) else if "%PARAMETER%"=="SW_SOURCES_DIR" (
   if "%VALUE%"=="" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_BUILD_DIR_DEFAULT" (
+) else if "%PARAMETER%"=="SW_BUILD_DIR" (
   if "%VALUE%"=="" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_INSTALL_DIR_DEFAULT" (
+) else if "%PARAMETER%"=="SW_INSTALL_DIR" (
   if "%VALUE%"=="" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_CONFIG_FILE_DEFAULT" (
+) else if "%PARAMETER%"=="SW_CONFIG_FILE" (
   if "%VALUE%"=="" goto :sw_validate_parameter_fail
-) else if "%PARAMETER%"=="SW_SWIFT_TEST_ENABLED_DEFAULT" (
+) else if "%PARAMETER%"=="SW_SWIFT_TEST_ENABLED" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
     goto :sw_validate_parameter_fail
   )
-) else if "%PARAMETER%"=="SW_DISPATCH_TEST_ENABLED_DEFAULT" (
+) else if "%PARAMETER%"=="SW_DISPATCH_TEST_ENABLED" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
     goto :sw_validate_parameter_fail
   )
-) else if "%PARAMETER%"=="SW_FOUNDATION_TEST_ENABLED_DEFAULT" (
+) else if "%PARAMETER%"=="SW_FOUNDATION_TEST_ENABLED" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
     goto :sw_validate_parameter_fail
   )
-) else if "%PARAMETER%"=="SW_OBJC_PATCH_ENABLED_DEFAULT" (
+) else if "%PARAMETER%"=="SW_OBJC_PATCH_ENABLED" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
     goto :sw_validate_parameter_fail
   )
-) else if "%PARAMETER%"=="SW_STDLIB_PATCH_ENABLED_DEFAULT" (
+) else if "%PARAMETER%"=="SW_STDLIB_PATCH_ENABLED" (
   if /i not "%VALUE%"=="YES" if /i not "%VALUE%"=="NO" (
     goto :sw_validate_parameter_fail
   )
@@ -405,5 +315,153 @@ if /i "%VALUE%"=="N" set VALUE=N
 )
 
 
+
+rem ###########################################################################
+:sw_validate_bool_input <parameter>
+setlocal enabledelayedexpansion
+
+set PARAMETER=%1
+set VALUE=!%PARAMETER%!
+
+if not "%VALUE%"=="Y" if not "%VALUE%"=="N" exit /b 1
+
+endlocal
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_branch
+set SW_ORIGINAL_VALUE=%SW_BRANCH_NUM%
+
+:sw_ask_branch_input
+echo Available branches:
+echo.  1. master
+echo.  2. 5.3
+echo.  3. 5.2
+
+set /p SW_BRANCH_NUM="Enter branch number to build (%SW_BRANCH_NUM%): "
+
+if [%SW_BRANCH_NUM%]==[1] (
+  set SW_SWIFT_BRANCH_SPEC=master
+) else if [%SW_BRANCH_NUM%]==[2] (
+  set SW_SWIFT_BRANCH_SPEC=5.3
+) else if [%SW_BRANCH_NUM%]==[3] (
+  set SW_SWIFT_BRANCH_SPEC=5.2
+) else (
+  set SW_BRANCH_NUM=%SW_ORIGINAL_VALUE%
+  goto sw_ask_branch_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_directories
+
+set /p SW_SOURCES_DIR="Enter directory for source files (%SW_SOURCES_DIR%): "
+set /p SW_BUILD_DIR="Enter directory for build output (%SW_BUILD_DIR%): "
+set /p SW_INSTALL_DIR="Enter directory to install into (%SW_INSTALL_DIR%): "
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_swift_patch
+set SW_ORIGINAL_VALUE=%SW_OBJC_PATCH_ENABLED%
+
+:sw_ask_swift_patch_input
+set /p SW_OBJC_PATCH_ENABLED="Apply @objc patch (%SW_OBJC_PATCH_ENABLED%)?: "
+call :sw_normalize_bool_input SW_OBJC_PATCH_ENABLED
+call :sw_validate_bool_input SW_OBJC_PATCH_ENABLED
+if errorlevel 1 (
+  set SW_OBJC_PATCH_ENABLED=%SW_ORIGINAL_VALUE%
+  goto sw_ask_swift_patch_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_stdlib_patch
+set SW_ORIGINAL_VALUE=%SW_STDLIB_PATCH_ENABLED%
+
+:sw_ask_stdlib_patch_input
+set /p SW_STDLIB_PATCH_ENABLED="Apply print flush patch (%SW_STDLIB_PATCH_ENABLED%)?: "
+call :sw_normalize_bool_input SW_STDLIB_PATCH_ENABLED
+call :sw_validate_bool_input SW_STDLIB_PATCH_ENABLED
+if errorlevel 1 (
+  set SW_STDLIB_PATCH_ENABLED=%SW_ORIGINAL_VALUE%
+  goto sw_ask_stdlib_patch_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_swift_test
+set SW_ORIGINAL_VALUE=%SW_SWIFT_TEST_ENABLED%
+
+:sw_ask_swift_test_input
+set /p SW_SWIFT_TEST_ENABLED="Enable Swift test (%SW_SWIFT_TEST_ENABLED%)?: "
+call :sw_normalize_bool_input SW_SWIFT_TEST_ENABLED
+call :sw_validate_bool_input SW_SWIFT_TEST_ENABLED
+if errorlevel 1 (
+  set SW_SWIFT_TEST_ENABLED=%SW_ORIGINAL_VALUE%
+  goto sw_ask_swift_test_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_dispatch_test
+set SW_ORIGINAL_VALUE=%SW_DISPATCH_TEST_ENABLED%
+
+:sw_ask_dispatch_test_input
+set /p SW_DISPATCH_TEST_ENABLED="Enable Dispatch test (%SW_DISPATCH_TEST_ENABLED%)?: "
+call :sw_normalize_bool_input SW_DISPATCH_TEST_ENABLED
+call :sw_validate_bool_input SW_DISPATCH_TEST_ENABLED
+if errorlevel 1 (
+  set SW_DISPATCH_TEST_ENABLED=%SW_ORIGINAL_VALUE%
+  goto sw_ask_dispatch_test_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_foundation_test
+set SW_ORIGINAL_VALUE=%SW_FOUNDATION_TEST_ENABLED%
+
+:sw_ask_foundation_test_input
+set /p SW_FOUNDATION_TEST_ENABLED="Enable Foundation test (%SW_FOUNDATION_TEST_ENABLED%)?: "
+call :sw_normalize_bool_input SW_FOUNDATION_TEST_ENABLED
+call :sw_validate_bool_input SW_FOUNDATION_TEST_ENABLED
+if errorlevel 1 (
+  set SW_FOUNDATION_TEST_ENABLED=%SW_ORIGINAL_VALUE%
+  goto sw_ask_foundation_test_input
+)
+
+exit /b
+
+
+
+rem ###########################################################################
+:sw_ask_config_file
+
+:sw_ask_config_file_input
+set /p SW_CONFIG_FILE="Enter configuration file name to save (%SW_CONFIG_FILE%): "
+if "%SW_CONFIG_FILE%"=="" (
+  goto sw_ask_config_file_input
+)
+
+exit /b
 
 endlocal
