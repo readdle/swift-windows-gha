f1798a567860955b82e480a3a2a967e466478ff6
Alexander Smarus <bender@readdle.com>
2020-07-26 13:49:49 +0300

    Support Readdle repos for SDK build (#14)

--- scripts/workflows/jobs/steps/icu/checkout-icu.cmd
+++ scripts/workflows/jobs/steps/icu/checkout-icu.cmd
@@ -1,1 +1,1 @@
-call scripts\tools\checkout.cmd --url=https://github.com/unicode-org/icu --dir=%SW_ICU_SOURCES_DIR% --ref=%SW_ICU_REF% --depth=1
+call scripts\tools\checkout.cmd --url=git://github.com/unicode-org/icu --dir=%SW_ICU_SOURCES_DIR% --ref=%SW_ICU_REF% --depth=1
