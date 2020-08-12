7e09425659ff4cd8c6246f7186795aca7e6a8761
Alexander Smarus <bender@readdle.com>
2020-08-12 21:50:16 +0300

    [sdk] support master branch for print-flush patch

--- scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
+++ scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
@@ -2,21 +2,29 @@ setlocal
 
 set SW_PUSHDIR=%CD%
 
+set SW_LOG_PATCH_INFO=%SW_LOG_INFO% --scope patch-stdlib
+set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-stdlib
+
+set SW_PRINT_FLUSH_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\print-flush-%SW_SWIFT_BRANCH_SPEC%.patch
+set SW_INSTALL_PDB_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\install-pdb.patch
+
 if [%SW_STDLIB_PATCH_ENABLED%]==[YES] (
-  echo Applying print-flush.patch
-  set SW_PRINT_FLUSH_PATCH=git apply --verbose %SW_WORKSPACE%\patch\swift\stdlib\print-flush.patch
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PRINT_FLUSH_PATCH%"
+  set SW_PRINT_FLUSH_PATCH=git apply --verbose %SW_PRINT_FLUSH_PATCH%
 ) else (
-  echo Skipping print-flush.patch
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PRINT_FLUSH_PATCH%"
   set SW_PRINT_FLUSH_PATCH=echo .
 )
 
 if [%SW_SWIFT_SDK_SPEC%]==[readdle] (
-  echo Applying install-pdb.patch
-  set SW_INSTALL_PDB_PATCH=git apply --verbose %SW_WORKSPACE%\patch\swift\stdlib\install-pdb.patch
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_INSTALL_PDB_PATCH%"
+  set SW_INSTALL_PDB_PATCH=git apply --verbose %SW_INSTALL_PDB_PATCH%
 ) else (
-  echo Skipping install-pdb.patch
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_INSTALL_PDB_PATCH%"
   set SW_INSTALL_PDB_PATCH=echo .
 )
+
+%SW_LOG_PATCH_INFO% --message="Working in %SW_SWIFT_SOURCES_DIR%"
 
 cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
@@ -25,6 +33,10 @@ cd /d %SW_SWIFT_SOURCES_DIR%^
  && %SW_INSTALL_PDB_PATCH%
 
 set SW_ERROR=%ERRORLEVEL%
+
+%SW_LOG_PATCH_INFO% --message="Restoring original working directory %SW_PUSHDIR%"
 cd /d %SW_PUSHDIR%
+
+if not %SW_ERROR% equ 0 %SW_LOG_PATCH_ERROR% --message="Patch failed"
 
 endlocal & exit /b %SW_ERROR%
