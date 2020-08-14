1c6831f0cfb462477027e8ec2aa74b1927106847
Konstiantyn Gominyuk <55390984+kgominyuk@users.noreply.github.com>
2020-08-14 12:41:56 +0300

    Add print-backtrace patch (#18)

--- scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
+++ scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
@@ -7,6 +7,7 @@ set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-stdlib
 
 set SW_PRINT_FLUSH_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\print-flush-%SW_SWIFT_BRANCH_SPEC%.patch
 set SW_INSTALL_PDB_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\install-pdb.patch
+set SW_PRINT_BACKTRACE_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\print-backtrace.patch
 
 if [%SW_STDLIB_PATCH_ENABLED%]==[YES] (
   %SW_LOG_PATCH_INFO% --message="Will apply %SW_PRINT_FLUSH_PATCH%"
@@ -19,9 +20,13 @@ if [%SW_STDLIB_PATCH_ENABLED%]==[YES] (
 if [%SW_SWIFT_SDK_SPEC%]==[readdle] (
   %SW_LOG_PATCH_INFO% --message="Will apply %SW_INSTALL_PDB_PATCH%"
   set SW_INSTALL_PDB_PATCH=git apply --verbose %SW_INSTALL_PDB_PATCH%
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PRINT_BACKTRACE_PATCH%"
+  set SW_PRINT_BACKTRACE_PATCH=git apply --verbose %SW_PRINT_BACKTRACE_PATCH%
 ) else (
   %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_INSTALL_PDB_PATCH%"
   set SW_INSTALL_PDB_PATCH=echo .
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PRINT_BACKTRACE_PATCH%"
+  set SW_PRINT_BACKTRACE_PATCH=echo .
 )
 
 %SW_LOG_PATCH_INFO% --message="Working in %SW_SWIFT_SOURCES_DIR%"
@@ -30,7 +35,8 @@ cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'builder@lxbndr.org'^
  && %SW_PRINT_FLUSH_PATCH%^
- && %SW_INSTALL_PDB_PATCH%
+ && %SW_INSTALL_PDB_PATCH%^
+ && %SW_PRINT_BACKTRACE_PATCH%
 
 set SW_ERROR=%ERRORLEVEL%
 
