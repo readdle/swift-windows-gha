fe93f371610886c0fa64fcc49b3d55a6a1d8342a
Alexander Smarus <bender@readdle.com>
2020-09-04 02:16:39 +0300

    [5.2] patch 5.2 for latest MSVC compatibility

--- scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
+++ scripts/workflows/jobs/steps/sdk/patch-stdlib.cmd
@@ -5,28 +5,37 @@ set SW_PUSHDIR=%CD%
 set SW_LOG_PATCH_INFO=%SW_LOG_INFO% --scope patch-stdlib
 set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-stdlib
 
-set SW_PRINT_FLUSH_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\print-flush-%SW_SWIFT_BRANCH_SPEC%.patch
-set SW_INSTALL_PDB_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\install-pdb.patch
-set SW_PRINT_BACKTRACE_PATCH=%SW_WORKSPACE%\patch\swift\stdlib\print-backtrace.patch
+set SW_PATCH_PRINT_FLUSH=%SW_WORKSPACE%\patch\swift\stdlib\print-flush-%SW_SWIFT_BRANCH_SPEC%.patch
+set SW_PATCH_INSTALL_PDB=%SW_WORKSPACE%\patch\swift\stdlib\install-pdb.patch
+set SW_PATCH_PRINT_BACKTRACE=%SW_WORKSPACE%\patch\swift\stdlib\print-backtrace.patch
+set SW_PATCH_CONDITIONAL_EXPLCIT=%SW_WORKSPACE%\patch\swift\stdlib\21a4b135529dc83efb4065333e12328c1b77054a.patch
 
 if [%SW_STDLIB_PATCH_ENABLED%]==[YES] (
-  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PRINT_FLUSH_PATCH%"
-  set SW_PRINT_FLUSH_PATCH=git apply --verbose %SW_PRINT_FLUSH_PATCH%
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_PRINT_FLUSH%"
+  set SW_PATCH_PRINT_FLUSH=git apply --verbose %SW_PATCH_PRINT_FLUSH%
 ) else (
-  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PRINT_FLUSH_PATCH%"
-  set SW_PRINT_FLUSH_PATCH=echo .
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_PRINT_FLUSH%"
+  set SW_PATCH_PRINT_FLUSH=echo .
 )
 
 if [%SW_SWIFT_SDK_SPEC%]==[readdle] (
-  %SW_LOG_PATCH_INFO% --message="Will apply %SW_INSTALL_PDB_PATCH%"
-  set SW_INSTALL_PDB_PATCH=git apply --verbose %SW_INSTALL_PDB_PATCH%
-  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PRINT_BACKTRACE_PATCH%"
-  set SW_PRINT_BACKTRACE_PATCH=git apply --verbose %SW_PRINT_BACKTRACE_PATCH%
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_INSTALL_PDB%"
+  set SW_PATCH_INSTALL_PDB=git apply --verbose %SW_PATCH_INSTALL_PDB%
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_PRINT_BACKTRACE%"
+  set SW_PATCH_PRINT_BACKTRACE=git apply --verbose %SW_PATCH_PRINT_BACKTRACE%
 ) else (
-  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_INSTALL_PDB_PATCH%"
-  set SW_INSTALL_PDB_PATCH=echo .
-  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PRINT_BACKTRACE_PATCH%"
-  set SW_PRINT_BACKTRACE_PATCH=echo .
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_INSTALL_PDB%"
+  set SW_PATCH_INSTALL_PDB=echo.
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_PRINT_BACKTRACE%"
+  set SW_PATCH_PRINT_BACKTRACE=echo.
+)
+
+if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_CONDITIONAL_EXPLCIT%"
+  set SW_PATCH_CONDITIONAL_EXPLCIT=git apply %SW_PATCH_CONDITIONAL_EXPLCIT%
+) else (
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_CONDITIONAL_EXPLCIT%"
+  set SW_PATCH_CONDITIONAL_EXPLCIT=echo.
 )
 
 %SW_LOG_PATCH_INFO% --message="Working in %SW_SWIFT_SOURCES_DIR%"
@@ -34,9 +43,10 @@ if [%SW_SWIFT_SDK_SPEC%]==[readdle] (
 cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'builder@lxbndr.org'^
- && %SW_PRINT_FLUSH_PATCH%^
- && %SW_INSTALL_PDB_PATCH%^
- && %SW_PRINT_BACKTRACE_PATCH%
+ && %SW_PATCH_PRINT_FLUSH%^
+ && %SW_PATCH_INSTALL_PDB%^
+ && %SW_PATCH_PRINT_BACKTRACE%^
+ && %SW_PATCH_CONDITIONAL_EXPLCIT%
 
 set SW_ERROR=%ERRORLEVEL%
 
--- scripts/workflows/jobs/steps/toolchain/patch-swift.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-swift.cmd
@@ -5,12 +5,19 @@ if not [%SW_OBJC_PATCH_ENABLED%]==[YES] (
 
 setlocal
 
+if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
+  set SW_PATCH_CONDITIONAL_EXPLCIT=git apply --verbose %SW_WORKSPACE%\patch\swift\21a4b135529dc83efb4065333e12328c1b77054a.patch
+) else (
+  set SW_PATCH_CONDITIONAL_EXPLCIT=echo.
+)
+
 set SW_PUSHDIR=%CD%
 
 cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'builder@lxbndr.org'^
- && git apply --verbose %SW_WORKSPACE%\patch\swift\disable-objc-%SW_SWIFT_BRANCH_SPEC%.patch
+ && git apply --verbose %SW_WORKSPACE%\patch\swift\disable-objc-%SW_SWIFT_BRANCH_SPEC%.patch^
+ && %SW_PATCH_CONDITIONAL_EXPLCIT%
 
 set SW_ERROR=%ERRORLEVEL%
 cd /d %SW_PUSHDIR%
