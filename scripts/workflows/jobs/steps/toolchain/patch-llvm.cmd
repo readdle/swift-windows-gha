e5de66296478c28f6df8dd327d7f4ccf4ad4824c
Alexander Smarus <bender@readdle.com>
2020-09-04 21:45:45 +0300

    [5.2] patch llvm for MSVC compatibility

--- scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
@@ -1,4 +1,4 @@
-setlocal
+setlocal enabledelayedexpansion
 
 set SW_PUSHDIR=%CD%
 
@@ -6,7 +6,8 @@ if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
   cd /d %SW_LLVM_PROJECT_SOURCES_DIR%^
    && git config user.name 'builder'^
    && git config user.email 'builder@lxbndr.org'^
-   && git apply --verbose %SW_WORKSPACE%\patch\llvm-project\b288d90b39f4b905c02092a9bfcfd6d78f99b191.patch
+   && git apply --verbose %SW_WORKSPACE%\patch\llvm-project\b288d90b39f4b905c02092a9bfcfd6d78f99b191.patch^
+   && git apply --verbose %SW_WORKSPACE%\patch\llvm-project\762d17e112ba90e677daef60fafaa82236f28064_type_traits.patch
 )
 
 set SW_ERROR=%ERRORLEVEL%
