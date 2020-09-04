b29260eb85d6793772a80645966aa7915b47324f
Alexander Smarus <bender@readdle.com>
2020-09-04 12:42:48 +0300

    [5.2] enable patch for MSVC compatibility

--- scripts/workflows/jobs/steps/toolchain/patch-swift.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-swift.cmd
@@ -1,14 +1,25 @@
-if not [%SW_OBJC_PATCH_ENABLED%]==[YES] (
-    echo Skipping Swift patches
-    goto :eof
-)
-
 setlocal
 
+set SW_LOG_PATCH_INFO=%SW_LOG_INFO% --scope patch-swift
+set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-swift
+
+set SW_PATCH_CONDITIONAL_EXPLCIT=%SW_WORKSPACE%\patch\swift\21a4b135529dc83efb4065333e12328c1b77054a.patch
+set SW_PATCH_DISABLE_OBJC=%SW_WORKSPACE%\patch\swift\disable-objc-%SW_SWIFT_BRANCH_SPEC%.patch
+
 if [%SW_SWIFT_BRANCH_SPEC%]==[5.2] (
-  set SW_PATCH_CONDITIONAL_EXPLCIT=git apply --verbose %SW_WORKSPACE%\patch\swift\21a4b135529dc83efb4065333e12328c1b77054a.patch
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_CONDITIONAL_EXPLCIT%"
+  set SW_PATCH_CONDITIONAL_EXPLCIT=git apply --verbose %SW_PATCH_CONDITIONAL_EXPLCIT%
 ) else (
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_CONDITIONAL_EXPLCIT%"
   set SW_PATCH_CONDITIONAL_EXPLCIT=echo.
+)
+
+if [%SW_OBJC_PATCH_ENABLED%]==[YES] (
+  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_DISABLE_OBJC%"
+  set SW_PATCH_DISABLE_OBJC=git apply --verbose %SW_PATCH_DISABLE_OBJC%
+) else (
+  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_DISABLE_OBJC%"
+  set SW_PATCH_DISABLE_OBJC=echo.
 )
 
 set SW_PUSHDIR=%CD%
@@ -16,7 +27,7 @@ set SW_PUSHDIR=%CD%
 cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'builder@lxbndr.org'^
- && git apply --verbose %SW_WORKSPACE%\patch\swift\disable-objc-%SW_SWIFT_BRANCH_SPEC%.patch^
+ && %SW_PATCH_DISABLE_OBJC%^
  && %SW_PATCH_CONDITIONAL_EXPLCIT%
 
 set SW_ERROR=%ERRORLEVEL%
