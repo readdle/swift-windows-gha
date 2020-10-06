66b0aa002e2b6801091bb61f043d820552c5f05a
Alexander Smarus <bender@readdle.com>
2020-10-06 16:54:29 +0300

    fix test environment setup

--- scripts/workflows/jobs/steps/sdk/configure-foundation-test-environment.cmd
+++ scripts/workflows/jobs/steps/sdk/configure-foundation-test-environment.cmd
@@ -3,6 +3,6 @@ setlocal enabledelayedexpansion
 %SW_PRE_STEP% --scope="sdk" --name="Configure Foundation Test Environment" --flag=SW_SKIP_SDK_FOUNDATION_TEST
 if errorlevel 1 exit /b 0
 
-call scripts\tools\set-env.cmd PATH "%SW_ICU_PATH%\usr\bin;%SW_STDLIB_BUILD_DIR%\bin;%SW_DISPATCH_BUILD_DIR%;%SW_FOUNDATION_BUILD_DIR%\Foundation;%SW_XCTEST_BUILD_DIR%;%PATH%;%ProgramFiles%\Git\usr\bin"
-
 endlocal
+
+call scripts\tools\set-env.cmd PATH "%SW_ICU_PATH%\usr\bin;%SW_STDLIB_BUILD_DIR%\bin;%SW_DISPATCH_BUILD_DIR%;%SW_FOUNDATION_BUILD_DIR%\Foundation;%SW_XCTEST_BUILD_DIR%;%PATH%;%ProgramFiles%\Git\usr\bin"
--- scripts/workflows/jobs/steps/toolchain/configure-test-environment.cmd
+++ scripts/workflows/jobs/steps/toolchain/configure-test-environment.cmd
@@ -3,6 +3,6 @@ setlocal enabledelayedexpansion
 %SW_PRE_STEP% --scope="toolchain" --name="Configure Test Environment" --flag=SW_SKIP_TOOLCHAIN_SWIFT_TEST
 if errorlevel 1 exit /b 0
 
-call scripts\tools\set-env.cmd PATH "%SW_ICU_PATH%\usr\bin;%SW_TOOLCHAIN_INSTALL_DIR%\bin;%PATH%;%ProgramFiles%\Git\usr\bin"
-
 endlocal
+
+call scripts\tools\set-env.cmd PATH "%SW_ICU_PATH%\usr\bin;%SW_TOOLCHAIN_INSTALL_DIR%\bin;%PATH%;%ProgramFiles%\Git\usr\bin"
