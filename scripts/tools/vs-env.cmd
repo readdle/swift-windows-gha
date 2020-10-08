64eb38fa6c8b9f7eb92d2436f761ffb701a4fe95
Alexander Smarus <bender@readdle.com>
2020-10-08 23:59:35 +0300

    replace deprecated workflow commands

--- scripts/tools/vs-env.cmd
+++ scripts/tools/vs-env.cmd
@@ -7,8 +7,4 @@ for /f "usebackq delims=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Stu
 
 if not defined GITHUB_ACTIONS goto :eof
 
-setlocal
-for /f "delims== tokens=1,2" %%a in ('set') do (
-    echo ::set-env name=%%a::%%b
-)
-endlocal
+set>>%GITHUB_ENV%
