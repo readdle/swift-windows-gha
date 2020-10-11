ac02fd25443397c3dc2ff0175eefdfa31a3093f9
Alexander Smarus <bender@readdle.com>
2020-10-11 23:48:25 +0300

    rename files related swift argument parser

--- scripts/workflows/jobs/devtools.cmd
+++ scripts/workflows/jobs/devtools.cmd
@@ -16,7 +16,7 @@ call %SW_STEPS_DIR%\configure-build-environment.cmd^
  && call %SW_STEPS_DIR%\checkout-llbuild.cmd^
  && call %SW_STEPS_DIR%\checkout-tsc.cmd^
  && call %SW_STEPS_DIR%\checkout-yams.cmd^
- && call %SW_STEPS_DIR%\checkout-swift-argument-parser.cmd^
+ && call %SW_STEPS_DIR%\checkout-sap.cmd^
  && call %SW_STEPS_DIR%\checkout-swift-driver.cmd^
  && call %SW_STEPS_DIR%\configure-llbuild.cmd^
  && call %SW_STEPS_DIR%\build-llbuild.cmd^
@@ -27,9 +27,9 @@ call %SW_STEPS_DIR%\configure-build-environment.cmd^
  && call %SW_STEPS_DIR%\configure-yams.cmd^
  && call %SW_STEPS_DIR%\build-yams.cmd^
  && call %SW_STEPS_DIR%\install-yams.cmd^
- && call %SW_STEPS_DIR%\configure-swift-argument-parser.cmd^
- && call %SW_STEPS_DIR%\build-swift-argument-parser.cmd^
- && call %SW_STEPS_DIR%\install-swift-argument-parser.cmd^
+ && call %SW_STEPS_DIR%\configure-sap.cmd^
+ && call %SW_STEPS_DIR%\build-sap.cmd^
+ && call %SW_STEPS_DIR%\install-sap.cmd^
  && call %SW_STEPS_DIR%\configure-swift-driver.cmd^
  && call %SW_STEPS_DIR%\build-swift-driver.cmd^
  && call %SW_STEPS_DIR%\install-swift-driver.cmd
