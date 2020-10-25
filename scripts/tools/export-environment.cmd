271c0a9230b3ec18d1e616e4b2e04749a17476e0
Alexander Smarus <bender@readdle.com>
2020-10-25 20:19:23 +0200

    disable environment export for script build

--- scripts/tools/export-environment.cmd
+++ scripts/tools/export-environment.cmd
@@ -1,3 +1,6 @@
+:: Build script has global environment and needs no special handling of environment variables
+if "%SW_BUILD_ENVIRONMENT%"=="script" goto :eof
+
 :: No sense to try GitHub Actions Workflow command when running outside of Github Actions
 if not defined GITHUB_ACTIONS goto :eof
 
--- scripts/tools/vs-env.cmd
+++ scripts/tools/vs-env.cmd
@@ -5,6 +5,8 @@ for /f "usebackq delims=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Stu
     call "%%i"\Common7\Tools\vsdevcmd.bat %*
 )
 
+if "%SW_BUILD_ENVIRONMENT%"=="script" goto :eof
+
 if not defined GITHUB_ACTIONS goto :eof
 
 set>%GITHUB_ENV%
