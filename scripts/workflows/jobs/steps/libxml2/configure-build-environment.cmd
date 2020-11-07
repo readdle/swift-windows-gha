ed0ca90fbcc319056d0bedd95720558efad5f3c3
Alexander Smarus <bender@readdle.com>
2020-11-07 23:21:24 +0200

    add libxml2 workflow

--- scripts/workflows/jobs/steps/libxml2/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/libxml2/configure-build-environment.cmd
@@ -1,6 +1,14 @@
-call scripts\tools\vs-env.cmd -arch=x64 -host_arch=x64
+%SW_SET_ENV% SW_LOG_XML2_INFO "%SW_LOG_INFO% --scope libxml2"
+%SW_SET_ENV% SW_LOG_XML2_WARNING "%SW_LOG_WARNING% --scope libxml2"
 
-call scripts\tools\set-env.cmd SW_XML2_REF cmake
-call scripts\tools\set-env.cmd SW_XML2_SOURCES_DIR %SW_SOURCES_DIR%\libxml2
-call scripts\tools\set-env.cmd SW_XML2_BUILD_DIR %SW_BUILD_DIR%\libxml2
-call scripts\tools\set-env.cmd SW_XML2_INSTALL_DIR %SW_INSTALL_DIR%\Library\libxml2-%SW_XML2_VERSION%\usr
+%SW_LOG_XML2_INFO% --message="Configuring build environment"
+
+%SW_SET_ENV% SW_XML2_REF cmake
+%SW_SET_ENV% SW_XML2_SOURCES_DIR %SW_SOURCES_DIR%\libxml2
+%SW_SET_ENV% SW_XML2_BUILD_DIR %SW_BUILD_DIR%\libxml2
+%SW_SET_ENV% SW_XML2_INSTALL_DIR %SW_INSTALL_DIR%\Library\libxml2-%SW_XML2_VERSION%\usr
+
+%SW_LOG_XML2_INFO% --prefix="Git ref:           " --message="%SW_XML2_REF%"
+%SW_LOG_XML2_INFO% --prefix="Sources directory: " --message="%SW_XML2_SOURCES_DIR%"
+%SW_LOG_XML2_INFO% --prefix="Build directory:   " --message="%SW_XML2_BUILD_DIR%"
+%SW_LOG_XML2_INFO% --prefix="Install directory: " --message="%SW_XML2_INSTALL_DIR%"
