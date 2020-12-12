340cc62e0ad119313d4245a211414bd0f40a5513
Alexander Smarus <bender@readdle.com>
2020-12-12 15:27:44 +0200

    don't assume environment side effects of vs-env script

--- scripts/tools/vs-env.cmd
+++ scripts/tools/vs-env.cmd
@@ -4,9 +4,3 @@ REM https://github.com/microsoft/vswhere/wiki/Find-VC
 for /f "usebackq delims=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
     call "%%i"\Common7\Tools\vsdevcmd.bat %*
 )
-
-if "%SW_BUILD_ENVIRONMENT%"=="script" goto :eof
-
-if not defined GITHUB_ACTIONS goto :eof
-
-set>%GITHUB_ENV%
