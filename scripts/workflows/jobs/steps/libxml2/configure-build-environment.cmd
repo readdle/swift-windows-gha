064f48a9ba43f3b4a2f495fcf8972f58af86001e
Alexander Smarus <bender@readdle.com>
2020-12-13 22:36:27 +0200

    fix libxml2 ref

--- scripts/workflows/jobs/steps/libxml2/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/libxml2/configure-build-environment.cmd
@@ -3,7 +3,7 @@ set "SW_LOG_XML2_WARNING=%SW_LOG_WARNING% --scope libxml2"
 
 %SW_LOG_XML2_INFO% --message="Configuring build environment"
 
-set SW_XML2_REF cmake
+set SW_XML2_REF=cmake
 set "SW_XML2_SOURCES_DIR=%SW_SOURCES_DIR%\libxml2"
 set "SW_XML2_BUILD_DIR=%SW_BUILD_DIR%\libxml2"
 set "SW_XML2_INSTALL_DIR=%SW_INSTALL_DIR%\Library\libxml2-%SW_XML2_VERSION%\usr"
