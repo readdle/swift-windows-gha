a4489e58f534311d59a936eb33ede9076f13d01e
Alexander Smarus <bender@readdle.com>
2021-01-11 15:30:12 +0200

    bump sqlite to v3.34.0

--- scripts/workflows/jobs/steps/sqlite/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/sqlite/configure-build-environment.cmd
@@ -1,9 +1,6 @@
 %SW_LOG_SQLITE_INFO% --prefix="Starting step:" --message="Configure Build Environment"
 
-set SW_SQLITE_RELEASE=3300100
-set SW_SQLITE_VERSION=3.30.1
-
-set SW_SQLITE_URL=https://sqlite.org/2019/sqlite-amalgamation-%SW_SQLITE_RELEASE%.zip
+set SW_SQLITE_URL=https://sqlite.org/2020/sqlite-amalgamation-%SW_SQLITE_RELEASE%.zip
 set "SW_SQLITE_SOURCES_DIR=%SW_SOURCES_DIR%\sqlite-amalgamation-%SW_SQLITE_RELEASE%"
 set "SW_SQLITE_BUILD_DIR=%SW_BUILD_DIR%\sqlite"
 set "SW_SQLITE_INSTALL_DIR=%SW_INSTALL_DIR%\Library\sqlite-%SW_SQLITE_VERSION%\usr"
