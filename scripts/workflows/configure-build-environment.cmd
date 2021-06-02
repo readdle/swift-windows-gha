e3d6a0cb4a3f6591d4d035c82c985c350c0f3d68
Alexander Smarus <bender@readdle.com>
2021-06-03 02:17:23 +0300

    enable debug logging

--- scripts/workflows/configure-build-environment.cmd
+++ scripts/workflows/configure-build-environment.cmd
@@ -27,9 +27,10 @@ set SW_PLUTIL=powershell -ExecutionPolicy Bypass "%SW_WORKSPACE%\scripts\tools\p
 %SW_LOG_BUILD_INFO% --prefix="pre-job command:         " --message="%SW_PRE_JOB%"
 
 %SW_LOG_BUILD_INFO% --message="Initializing Visual Studio environment"
+@echo on
 if "%SW_SWIFT_BRANCH_SPEC%"=="5.3" set VC_VERSION_ARG=-vcvars_ver=14.28.29910
 call "%SW_WORKSPACE%\scripts\tools\vs-env.cmd" -arch=x64 -host_arch=x64 %VC_VERSION_ARG%
-
+@echo off
 exit /b 0
 
 
