07d1446739774fb7f352916b850abdd614549001
Alexander Smarus <bender@readdle.com>
2021-06-04 11:15:45 +0300

    Update install-compiler-runtime.cmd

--- scripts/workflows/jobs/steps/toolchain/install-compiler-runtime.cmd
+++ scripts/workflows/jobs/steps/toolchain/install-compiler-runtime.cmd
@@ -1,1 +1,1 @@
-cmake --build %SW_TOOLCHAIN_BUILD_DIR% --target install-clang_rt.profile-x86_64-stripped
+cmake --build %SW_TOOLCHAIN_BUILD_DIR%\runtimes\runtimes-bins --target install-clang_rt.profile-x86_64-stripped
