8b13d1fac723b4b13013ae75d46cf305d38fe7cb
Alexander Smarus <bender@readdle.com>
2021-06-07 21:44:53 +0300

    don't delete important sources

--- scripts/workflows/jobs/steps/toolchain/clean-toolchain.cmd
+++ scripts/workflows/jobs/steps/toolchain/clean-toolchain.cmd
@@ -1,7 +1,6 @@
 %SW_LOG_TOOLCHAIN_INFO% --message="Cleaning..."
-rmdir /q /s %SW_LLVM_PROJECT_SOURCES_DIR%
+rmdir /q /s %SW_LLVM_SOURCES_DIR%
 rmdir /q /s %SW_CMARK_SOURCES_DIR%
-rmdir /q /s %SW_SWIFT_SOURCES_DIR%
 rmdir /q /s %SW_LLVM_TOOLS_BUILD_DIR%
 rmdir /q /s %SW_TOOLCHAIN_BUILD_DIR%
 
