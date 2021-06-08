7cac6780b2ad10f185ef8734a537cc8d608f2913
Alexander Smarus <bender@readdle.com>
2021-06-08 10:41:52 +0300

    don't delete llvm sources directory

--- scripts/workflows/jobs/steps/toolchain/clean-toolchain.cmd
+++ scripts/workflows/jobs/steps/toolchain/clean-toolchain.cmd
@@ -1,5 +1,4 @@
 %SW_LOG_TOOLCHAIN_INFO% --message="Cleaning..."
-rmdir /q /s %SW_LLVM_SOURCES_DIR%
 rmdir /q /s %SW_CMARK_SOURCES_DIR%
 rmdir /q /s %SW_LLVM_TOOLS_BUILD_DIR%
 rmdir /q /s %SW_TOOLCHAIN_BUILD_DIR%
