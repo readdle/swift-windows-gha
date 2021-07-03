f09230636aa639062bc37d6631647d9c511a9c0c
Alexander Smarus <bender@readdle.com>
2021-07-03 23:54:29 +0300

    adjust VS installation search

--- scripts/tools/vs-env.cmd
+++ scripts/tools/vs-env.cmd
@@ -1,6 +1,7 @@
 if defined VSCMD_VER goto :eof
 
 REM https://github.com/microsoft/vswhere/wiki/Find-VC
-for /f "usebackq delims=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
+for /f "usebackq delims=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -products * -property installationPath`) do (
     call "%%i"\Common7\Tools\vsdevcmd.bat %*
+    exit /b
 )
