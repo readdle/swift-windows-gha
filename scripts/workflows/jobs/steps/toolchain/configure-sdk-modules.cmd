15f946e6453c0f65d9aae88431d95436e0a7f778
Alexander Smarus <bender@readdle.com>
2021-07-09 14:22:49 +0300

    force overwriting swift platform modules in Visual Studio directory

--- scripts/workflows/jobs/steps/toolchain/configure-sdk-modules.cmd
+++ scripts/workflows/jobs/steps/toolchain/configure-sdk-modules.cmd
@@ -1,7 +1,7 @@
-copy %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\ucrt.modulemap "%UniversalCRTSdkDir%\Include\%UCRTVersion%\ucrt\module.modulemap"^
- && copy %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\visualc.modulemap "%VCToolsInstallDir%\include\module.modulemap"^
- && copy %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\visualc.apinotes "%VCToolsInstallDir%\include\visualc.apinotes"^
- && copy %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\winsdk.modulemap "%UniversalCRTSdkDir%\Include\%UCRTVersion%\um\module.modulemap"
+copy /y %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\ucrt.modulemap "%UniversalCRTSdkDir%\Include\%UCRTVersion%\ucrt\module.modulemap"^
+ && copy /y %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\visualc.modulemap "%VCToolsInstallDir%\include\module.modulemap"^
+ && copy /y %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\visualc.apinotes "%VCToolsInstallDir%\include\visualc.apinotes"^
+ && copy /y %SW_SWIFT_SOURCES_DIR%\stdlib\public\Platform\winsdk.modulemap "%UniversalCRTSdkDir%\Include\%UCRTVersion%\um\module.modulemap"
 
 if errorlevel 1 (
     echo Failed to install clang modules for Visual Studio. Make sure you have Administrator access.
