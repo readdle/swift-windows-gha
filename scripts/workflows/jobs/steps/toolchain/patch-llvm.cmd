23c7c7770155a05736f84aa1ce44018c90b91e67
Alexander Smarus <bender@readdle.com>
2021-09-24 01:50:18 +0300

    [swift] add patch for compiler runtime on latest VS

--- scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
@@ -1,14 +1,19 @@
 setlocal enabledelayedexpansion
 
-:: No patches for LLVM yet
-goto :eof
+set SW_LOG_PATCH_INFO=%SW_LOG_INFO% --scope patch-swift
+set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-swift
+
+set SW_PATCH_CMAKE_MT=%SW_WORKSPACE%\patch\llvm-project\cmake-mt-%SW_SWIFT_BRANCH_SPEC%.patch
+
+%SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_CMAKE_MT%"
+set SW_PATCH_CMAKE_MT=git apply --verbose %SW_PATCH_CMAKE_MT%
 
 set SW_PUSHDIR=%CD%
 
 cd /d %SW_LLVM_PROJECT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'swift-builder@readdle.com'^
- && git apply --verbose XXX.patch
+ && %SW_PATCH_CMAKE_MT%
 
 set SW_ERROR=%ERRORLEVEL%
 cd /d %SW_PUSHDIR%
