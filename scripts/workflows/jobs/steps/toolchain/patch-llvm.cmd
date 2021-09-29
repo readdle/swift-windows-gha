9d2369fb14940855a342e8d8498b0b94130d43d5
Alexander Smarus <bender@readdle.com>
2021-09-29 12:51:32 +0300

    [swift] disable llvm patch and use caches for mt tool

--- scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
+++ scripts/workflows/jobs/steps/toolchain/patch-llvm.cmd
@@ -3,19 +3,21 @@ setlocal enabledelayedexpansion
 set SW_LOG_PATCH_INFO=%SW_LOG_INFO% --scope patch-swift
 set SW_LOG_PATCH_ERROR=%SW_LOG_ERROR% --scope patch-swift
 
-set SW_PATCH_CMAKE_MT=%SW_WORKSPACE%\patch\llvm-project\cmake-mt-%SW_SWIFT_BRANCH_SPEC%.patch
+%SW_LOG_PATCH_INFO% --message="No patches for LLVM"
 
-%SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_CMAKE_MT%"
-set SW_PATCH_CMAKE_MT=git apply --verbose %SW_PATCH_CMAKE_MT%
+rem set SW_PATCH_CMAKE_MT=%SW_WORKSPACE%\patch\llvm-project\cmake-mt-%SW_SWIFT_BRANCH_SPEC%.patch
 
-set SW_PUSHDIR=%CD%
+rem %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_CMAKE_MT%"
+rem set SW_PATCH_CMAKE_MT=git apply --verbose %SW_PATCH_CMAKE_MT%
 
-cd /d %SW_LLVM_PROJECT_SOURCES_DIR%^
- && git config user.name 'builder'^
- && git config user.email 'swift-builder@readdle.com'^
- && %SW_PATCH_CMAKE_MT%
+rem set SW_PUSHDIR=%CD%
 
-set SW_ERROR=%ERRORLEVEL%
-cd /d %SW_PUSHDIR%
+rem cd /d %SW_LLVM_PROJECT_SOURCES_DIR%^
+rem  && git config user.name 'builder'^
+rem  && git config user.email 'swift-builder@readdle.com'^
+rem  && %SW_PATCH_CMAKE_MT%
+
+rem set SW_ERROR=%ERRORLEVEL%
+rem cd /d %SW_PUSHDIR%
 
 endlocal & exit /b %SW_ERROR%
