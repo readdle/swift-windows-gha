80d0fef9ffa3f1d4d5b545203125903dcb60dbf0
Alexander Smarus <bender@readdle.com>
2022-01-24 14:23:12 +0200

    make rebuilds less painful

--- scripts/workflows/jobs/steps/sdk/install-foundation.cmd
+++ scripts/workflows/jobs/steps/sdk/install-foundation.cmd
@@ -3,6 +3,14 @@ setlocal enabledelayedexpansion
 %SW_PRE_STEP% --scope="sdk" --name="Install Foundation" --flag=SW_SKIP_SDK_FOUNDATION
 if errorlevel 1 exit /b 0
 
+if not "%SW_SWIFT_BRANCH_SPEC%"=="5.5" if not "%SW_SWIFT_BRANCH_SPEC%"=="main" goto do-install
+
+:: Clean previous installation
+for %%M in (Foundation, FoundationNetworking, FoundationXML) do (
+  if exist "%SW_SDK_INSTALL_DIR%\lib\swift\windows\x86_64\%%M.swiftmodule" rmdir /q /s "%SW_SDK_INSTALL_DIR%\lib\swift\windows\x86_64\%%M.swiftmodule"
+)
+
+:do-install
 cmake --build %SW_FOUNDATION_BUILD_DIR% --target install
 
 endlocal
--- scripts/workflows/jobs/steps/sdk/install-libdispatch.cmd
+++ scripts/workflows/jobs/steps/sdk/install-libdispatch.cmd
@@ -3,6 +3,12 @@ setlocal enabledelayedexpansion
 %SW_PRE_STEP% --scope="sdk" --name="Install libdispatch" --flag=SW_SKIP_SDK_DISPATCH
 if errorlevel 1 exit /b 0
 
+if not "%SW_SWIFT_BRANCH_SPEC%"=="5.5" if not "%SW_SWIFT_BRANCH_SPEC%"=="main" goto do-install
+
+:: Clean previous installation
+if exist "%SW_SDK_INSTALL_DIR%\lib\swift\windows\x86_64\Dispatch.swiftmodule" rmdir /q /s "%SW_SDK_INSTALL_DIR%\lib\swift\windows\x86_64\Dispatch.swiftmodule"
+
+:do-install
 cmake --build %SW_DISPATCH_BUILD_DIR% --target install
 
 endlocal
