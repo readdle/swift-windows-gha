05d2f6a1d971e77810b0c5a269cd9254c5ad2cdf
Alexander Smarus <bender@readdle.com>
2022-12-22 16:10:56 +0100

    respect preset refs for sdk dependencies

--- scripts/workflows/jobs/steps/sdk/configure-build-environment.cmd
+++ scripts/workflows/jobs/steps/sdk/configure-build-environment.cmd
@@ -23,11 +23,26 @@ if defined SW_GIT_TAG (
     set SW_FOUNDATION_REF=%SW_GIT_TAG%    
   )
 ) else (
-  if not defined SW_LLVM_REF call :sw_get_llvm_ref SW_LLVM_REF
-  if not defined SW_SWIFT_REF call :sw_get_ref SW_SWIFT_REF
-  if not defined SW_DISPATCH_REF call :sw_get_ref SW_DISPATCH_REF %SW_SDK_SPEC_PREFIX%
-  if not defined SW_SWIFT_SYNTAX_REF call :sw_get_ref SW_SWIFT_SYNTAX_REF
-  if not defined SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_REF call :sw_get_ref SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_REF swift/
+  if not defined SW_LLVM_REF (
+    %SW_LOG_JOB_INFO% --message-"llvm-project ref not set, using default"
+    call :sw_get_llvm_ref SW_LLVM_REF
+  )
+  if not defined SW_SWIFT_REF (
+    %SW_LOG_JOB_INFO% --message-"swift ref not set, using default"
+    call :sw_get_ref SW_SWIFT_REF
+  )
+  if not defined SW_DISPATCH_REF (
+    %SW_LOG_JOB_INFO% --message-"swift-corelibs-libdispatch ref not set, using default"
+    call :sw_get_ref call :sw_get_ref SW_DISPATCH_REF %SW_SDK_SPEC_PREFIX%
+  )
+  if not defined SW_SWIFT_SYNTAX_REF (
+    %SW_LOG_JOB_INFO% --message-"swift-syntax ref not set, using default"
+    call :sw_get_ref call :sw_get_ref SW_SWIFT_SYNTAX_REF
+  )
+  if not defined SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_REF (
+    %SW_LOG_JOB_INFO% --message-"swift-experimental-string-processing ref not set, using default"
+    call :sw_get_ref call :sw_get_ref SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_REF
+  )
 
   call :sw_get_ref SW_FOUNDATION_REF %SW_SDK_SPEC_PREFIX%
   call :sw_get_ref SW_XCTEST_REF
@@ -71,22 +86,26 @@ set CTEST_OUTPUT_ON_FAILURE=1
 
 set "PATH=%SW_TOOLCHAIN_DIR%\usr\bin;%PATH%"
 
-%SW_LOG_JOB_INFO% --prefix="Dispatch origin:         " --message="%SW_DISPATCH_ORIGIN_URL%"
-%SW_LOG_JOB_INFO% --prefix="Foundation origin:       " --message="%SW_FOUNDATION_ORIGIN_URL%"
-%SW_LOG_JOB_INFO% --prefix="LLVM ref:                " --message="%SW_LLVM_REF%"
-%SW_LOG_JOB_INFO% --prefix="Dispatch ref:            " --message="%SW_DISPATCH_REF%"
-%SW_LOG_JOB_INFO% --prefix="Swift ref:               " --message="%SW_SWIFT_REF%"
-%SW_LOG_JOB_INFO% --prefix="Foundation ref:          " --message="%SW_FOUNDATION_REF%"
-%SW_LOG_JOB_INFO% --prefix="XCTest ref:              " --message="%SW_XCTEST_REF%"
+%SW_LOG_JOB_INFO% --prefix="%SW_DISPATCH_REPO%:                         " --message="%SW_DISPATCH_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-cmark:                          " --message="%SW_CMARK_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-syntax:                         " --message="%SW_SWIFT_SYNTAX_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/llvm-project:                         " --message="%SW_LLVM_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift:                                " --message="%SW_SWIFT_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-experimental-string-processing: " --message="%SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-corelibs-foundation:            " --message="%SW_FOUNDATION_REF%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-corelibs-xctest:                " --message="%SW_XCTEST_REF%"
 %SW_LOG_JOB_INFO%
-%SW_LOG_JOB_INFO% --prefix="LLVM Project sources dir:" --message="%SW_LLVM_PROJECT_SOURCES_DIR%"
-%SW_LOG_JOB_INFO% --prefix="Swift sources dir:       " --message="%SW_SWIFT_SOURCES_DIR%"
-%SW_LOG_JOB_INFO% --prefix="Dispatch sources dir:    " --message="%SW_DISPATCH_SOURCES_DIR%"
-%SW_LOG_JOB_INFO% --prefix="Foundation sources dir:  " --message="%SW_FOUNDATION_SOURCES_DIR%"
-%SW_LOG_JOB_INFO% --prefix="XCTest sources dir:      " --message="%SW_XCTEST_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="%SW_DISPATCH_REPO% sources:              " --message="%SW_DISPATCH_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-cmark sources:               " --message="%SW_CMARK_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-syntax sources:              " --message="%SW_SWIFT_SYNTAX_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/llvm-project sources:              " --message="%SW_LLVM_PROJECT_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift sources:                     " --message="%SW_SWIFT_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-experimental-string-processing sources: " --message="%SW_SWIFT_EXPERIMENTAL_STRING_PROCESSING_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-corelibs-foundation sources: " --message="%SW_FOUNDATION_SOURCES_DIR%"
+%SW_LOG_JOB_INFO% --prefix="apple/swift-corelibs-xctest sources:     " --message="%SW_XCTEST_SOURCES_DIR%"
 %SW_LOG_JOB_INFO%
 %SW_LOG_JOB_INFO% --prefix="LLVM build dir:          " --message="%SW_LLVM_BUILD_DIR%"
-%SW_LOG_JOB_INFO% --prefix="Runtime build dir:       " --message="%SW_RUNTIME_BUILD_DIR%"
+%SW_LOG_JOB_INFO% --prefix="Swift Runtime build dir: " --message="%SW_RUNTIME_BUILD_DIR%"
 %SW_LOG_JOB_INFO% --prefix="Dispatch build dir:      " --message="%SW_DISPATCH_BUILD_DIR%"
 %SW_LOG_JOB_INFO% --prefix="Foundation build dir:    " --message="%SW_FOUNDATION_BUILD_DIR%"
 %SW_LOG_JOB_INFO% --prefix="XCTest build dir:        " --message="%SW_XCTEST_BUILD_DIR%"
