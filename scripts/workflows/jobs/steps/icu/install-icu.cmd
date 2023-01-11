98b049f5c02655ec3288b1e033de4d4e16ccaa93
Alexander Smarus <bender@readdle.com>
2023-01-11 10:52:58 +0100

    copy icu data link library

--- scripts/workflows/jobs/steps/icu/install-icu.cmd
+++ scripts/workflows/jobs/steps/icu/install-icu.cmd
@@ -1,1 +1,6 @@
 cmake --build %SW_ICU_BUILD_DIR% --target install
+
+if "%SW_SWIFT_SDK_SPEC%"=="readdle" (
+    %SW_LOG_JOB_INFO% --message="Manually copying %SW_ICU_BUILD_DIR%\icudt%SW_ICU_RELEASE%.lib"
+    copy %SW_ICU_BUILD_DIR%\icudt%SW_ICU_RELEASE%.lib %SW_ICU_INSTALL_DIR%\lib\icudt%SW_ICU_RELEASE%.lib
+)
