fd6009325f6072301cd3ba7210ec35d237ecd850
Alexander Smarus <bender@readdle.com>
2023-04-17 01:30:46 +0300

    more tolerant icu tools install

--- scripts/workflows/jobs/steps/icu-tools/install-icu-tools.cmd
+++ scripts/workflows/jobs/steps/icu-tools/install-icu-tools.cmd
@@ -5,14 +5,15 @@ setlocal enabledelayedexpansion
 set SW_ICU_TOOLS_INSTALL_DIR=%SW_INSTALL_DIR%\icu-tools-%SW_ICU_VERSION%
 
 mkdir %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\genbrk.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\gencfu.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\gencnval.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\gendict.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\genrb.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\gensprep.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\icupkg.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\makeconv.exe %SW_ICU_TOOLS_INSTALL_DIR%^
- && copy %SW_ICU_TOOLS_BUILD_DIR%\pkgdata.exe %SW_ICU_TOOLS_INSTALL_DIR%
+
+copy /Y %SW_ICU_TOOLS_BUILD_DIR%\genbrk.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\gencfu.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\gencnval.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\gendict.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\genrb.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\gensprep.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\icupkg.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\makeconv.exe %SW_ICU_TOOLS_INSTALL_DIR%^
+ && copy /Y %SW_ICU_TOOLS_BUILD_DIR%\pkgdata.exe %SW_ICU_TOOLS_INSTALL_DIR%
 
 endlocal
