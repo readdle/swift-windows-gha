2f43f7318b11e59b3dc5d8afb777f518fd08e0ef
builder <swift-builder@readdle.com>
2023-04-20 11:43:44 +0200

    disable unnecessary swift runtime patches

--- scripts/workflows/jobs/steps/sdk/patch-runtime.cmd
+++ scripts/workflows/jobs/steps/sdk/patch-runtime.cmd
@@ -20,26 +20,12 @@ if [%SW_RUNTIME_PATCH_ENABLED%]==[YES] (
   set SW_PATCH_PRINT_FLUSH=echo.
 )
 
-if [%SW_SWIFT_SDK_SPEC%]==[readdle] (
-  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_INSTALL_PDB%"
-  set SW_PATCH_INSTALL_PDB=git apply --verbose %SW_PATCH_INSTALL_PDB%
-  %SW_LOG_PATCH_INFO% --message="Will apply %SW_PATCH_PRINT_BACKTRACE%"
-  set SW_PATCH_PRINT_BACKTRACE=git apply --verbose %SW_PATCH_PRINT_BACKTRACE%
-) else (
-  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_INSTALL_PDB%"
-  set SW_PATCH_INSTALL_PDB=echo.
-  %SW_LOG_PATCH_INFO% --message="Will NOT apply %SW_PATCH_PRINT_BACKTRACE%"
-  set SW_PATCH_PRINT_BACKTRACE=echo.
-)
-
 %SW_LOG_PATCH_INFO% --message="Working in %SW_SWIFT_SOURCES_DIR%"
 
 cd /d %SW_SWIFT_SOURCES_DIR%^
  && git config user.name 'builder'^
  && git config user.email 'swift-builder@readdle.com'^
- && %SW_PATCH_PRINT_FLUSH%^
- && %SW_PATCH_INSTALL_PDB%^
- && %SW_PATCH_PRINT_BACKTRACE%
+ && %SW_PATCH_PRINT_FLUSH%
 
 set SW_ERROR=%ERRORLEVEL%
 
